2569a9549d128e91277831e82e74cf686e3ae4c1
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-08 06:58:20 -0300

    Apply Tky

--- kind-tyk/tyk.tf
+++ kind-tyk/tyk.tf
@@ -1,106 +1,19 @@
-resource "helm_release" "tyk_gateway" {
-  name = "tyk-gateway"
-  chart = "https://s3-sa-east-1.amazonaws.com/charts.kurtis/tyk-headless-0.4.1.tgz"
-  namespace = kubernetes_namespace.tyk_ingress.metadata[0].name
-  timeout = 120
+resource "helm_release" "tyk" {
+  name = "tyk"
+  chart = "${path.root}/charts/tyk-gateway"
+  version = "1.0.0"
+  namespace = kubernetes_namespace.gateway.metadata[0].name
+  timeout = 240
 
   values = [
     yamlencode({
-      "enableSharding" = true
-      "fullnameOverride" = "tyk-headless"
-      "gateway" = {
-        "affinity" = {}
-        "containerPort" = 8080
-        "extraEnvs" = []
-        "hostName" = "gateway.tykbeta.com"
-        "image" = {
-          "pullPolicy" = "IfNotPresent",
-          "repository" = "tykio/tyk-gateway",
-          "tag" = "latest"
-        }
-        "ingress" = {
-          "annotations" = {},
-          "enabled" = false,
-          "hosts" = [
-            "tyk-gw.local",],
-          "path" = "/",
-          "tls" = []
-        }
-        "kind" = "Deployment"
-        "nodeSelector" = {
-          "ingress-ready" = "true"
-        }
-        "replicaCount" = 1
-        "resources" = {}
-        "service" = {
-          "annotations" = {},
-          "externalTrafficPolicy" = "Local",
-          "port" = 80,
-          "type" = "NodePort"
-        }
-        "tags" = "ingress"
-        "tls" = false
-        "tolerations" = [
-          {
-            effect = "NoSchedule",
-            key = "node-role.kubernetes.io/master"
-          },]
-      }
-      "mongo" = {
-        "mongoURL" = "mongodb://root:${var.mongodb_pass}@tyk-mongo-mongodb.${helm_release.tyk_mongo.namespace}.svc.cluster.local:27017/tyk-dashboard?authSource=admin"
-        "useSSL" = false
-      }
-      "nameOverride" = ""
-      "pump" = {
-        "affinity" = {}
-        "annotations" = {}
-        "extraEnvs" = []
-        "image" = {
-          "pullPolicy" = "IfNotPresent",
-          "repository" = "tykio/tyk-pump-docker-pub",
-          "tag" = "latest"
-        }
-        "nodeSelector" = {}
-        "replicaCount" = 1
-        "resources" = {}
-        "tolerations" = []
-      }
-      "rbac" = true
-      "redis" = {
-        "host" = "tyk-redis-master.${helm_release.tyk_redis.namespace}.svc.cluster.local"
-        "port" = 6379
-        "pass" = var.redis_pass
-        "shardCount" = 128
-        "useSSL" = false
-      }
-      "secrets" = {
-        "APISecret" = "CHANGEME",
-        "OrgID" = "1"
-      }
-      "tyk_k8s" = {
-        "affinity" = {}
-        "image" = {
-          "pullPolicy" = "Always",
-          "repository" = "tykio/tyk-k8s",
-          "tag" = "headless"
-        }
-        "nodeSelector" = {}
-        "resources" = {}
-        "serviceMesh" = {
-          "enabled" = false
-        }
-        "tolerations" = []
-        "watchNamespaces" = []
-      }
+      "service" = { "type" = "NodePort" }
+      "hostPort" = { "enabled" = true }
+      "nodeSelector" = { "ingress-ready" = "true" }
     })
   ]
 
   depends_on = [
-    helm_release.tyk_mongo,
     helm_release.tyk_redis
   ]
 }
-//
-//output "values_community_edition" {
-//  value = yamldecode(file("values_community_edition.yaml"))
-//}
