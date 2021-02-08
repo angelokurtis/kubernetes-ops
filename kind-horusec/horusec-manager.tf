f83aca62c259958d4accd12cc91d7815cdfb5aae
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-08 14:09:44 -0300

    Configuring Manager environments

--- kind-horusec/horusec-manager.tf
+++ kind-horusec/horusec-manager.tf
@@ -5,6 +5,24 @@ resource "helm_release" "horusec_manager" {
   namespace = kubernetes_namespace.horusec.metadata[0].name
   timeout = 240
 
+  values = [
+    yamlencode({
+      "env" = [
+        { name = "REACT_APP_HORUSEC_ENDPOINT_API", value = "https://api-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_ANALYTIC", value = "https://analytic-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_ACCOUNT", value = "https://account-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_AUTH", value = "https://auth-horus-dev.zup.com.br" },
+      ]
+      "image" = { "pullPolicy" = "Always", "repository" = "horuszup/horusec-manager", "tag" = "v1.3.0" }
+      "ingress" = {
+        "enabled" = true
+        "hosts" = [ { host = "horus-dev.zup.com.br" paths = [ "/" ] } ]
+      }
+      "service" = { "port" = 8080 "targetPort" = 8080 "type" = "ClusterIP" }
+      "serviceAccount" = { "create" = true }
+    })
+  ]
+
   depends_on = [
     helm_release.mongodb,
     helm_release.rabbit
