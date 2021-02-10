908a43b0d865132d17218bf7a48968367bd4d2ba
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-10 11:49:39 -0300

    Install manager with proper environment variables

--- kind-horusec/horusec-manager.tf
+++ kind-horusec/horusec-manager.tf
@@ -1,22 +1,23 @@
-resource "helm_release" "horusec_manager" {
-  name = "horusec-manager"
+resource "helm_release" "manager" {
+  name = "manager"
   // TODO use a proper repository
-  chart = "/home/tiagoangelo/wrkspc/github.com/ZupIT/horusec/horusec-manager/deployments/helm/horusec-manager"
+  chart = "${var.horusec_project_path}/horusec-manager/deployments/helm/horusec-manager"
   namespace = kubernetes_namespace.horusec.metadata[0].name
   timeout = 240
 
   values = [
     yamlencode({
+      "fullnameOverride" = "manager"
       "env" = [
-        { name = "REACT_APP_HORUSEC_ENDPOINT_API", value = "http://api-horus-dev.zup.com.br" },
-        { name = "REACT_APP_HORUSEC_ENDPOINT_ANALYTIC", value = "http://analytic-horus-dev.zup.com.br" },
-        { name = "REACT_APP_HORUSEC_ENDPOINT_ACCOUNT", value = "http://account-horus-dev.zup.com.br" },
-        { name = "REACT_APP_HORUSEC_ENDPOINT_AUTH", value = "http://auth-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_API", value = "http:\\\\/\\\\/api-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_ANALYTIC", value = "http:\\\\/\\\\/analytic-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_ACCOUNT", value = "http:\\\\/\\\\/account-horus-dev.zup.com.br" },
+        { name = "REACT_APP_HORUSEC_ENDPOINT_AUTH", value = "http:\\\\/\\\\/auth-horus-dev.zup.com.br" },
       ]
       "image" = { "pullPolicy" = "Always", "repository" = "horuszup/horusec-manager", "tag" = "v1.7.1" }
       "ingress" = {
         "enabled" = true
-        "hosts" = [ { host = "horus-dev.zup.com.br", paths = [ "/" ] } ]
+        "hosts" = [ { host = "horus-dev.zup.com.br", paths = [ "/"] } ]
         "annotations" = { "kubernetes.io/ingress.class" = "nginx" }
       }
       "service" = { "port" = 8080, "targetPort" = 8080, "type" = "ClusterIP" }
