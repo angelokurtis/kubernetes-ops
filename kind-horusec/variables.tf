5e20b5dbf620714888cb753b48549711e5677da6
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-10 11:46:39 -0300

    Using project path as variable

--- kind-horusec/variables.tf
+++ kind-horusec/variables.tf
@@ -1,7 +1,14 @@
 variable "mongodb_user" {
+  type = string
   default = "tiago.angelo"
 }
 
 variable "mongodb_pass" {
+  type = string
   default = "m0n90d8_p455"
+}
+
+variable "horusec_project_path" {
+  type = string
+  default = "/home/tiagoangelo/wrkspc/github.com/ZupIT/horusec"
 }
