2195dc793743f0d747a6a37baf442f44e37c19d2
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-13 15:53:14 -0300

    Not exposing Tyk ports

--- kind-tyk/kind.tf
+++ kind-tyk/kind.tf
@@ -9,19 +9,6 @@ resource "kind_cluster" "tyk" {
     node {
       role = "control-plane"
       image = "kindest/node:v1.18.15"
-
-      kubeadm_config_patches = [
-        yamlencode({
-          "kind" = "InitConfiguration"
-          "nodeRegistration" = { "kubeletExtraArgs" = { "node-labels" = "ingress-ready=true" } }
-        }),
-      ]
-
-      extra_port_mappings {
-        container_port = 8080
-        host_port = 8080
-        protocol = "TCP"
-      }
     }
   }
 }
