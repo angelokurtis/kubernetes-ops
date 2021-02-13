da8e6ec6cfc7f72a6dc14ba34358470c217c5e1f
Tiago Angelo <kurtis.angelo@gmail.com>
2021-02-13 15:53:03 -0300

    Updating Redis chart version

--- kind-tyk/redis.tf
+++ kind-tyk/redis.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "tyk_redis" {
   name = "tyk-redis"
-  chart = "https://charts.bitnami.com/bitnami/redis-12.6.4.tgz"
+  chart = "https://charts.bitnami.com/bitnami/redis-12.7.4.tgz"
   namespace = kubernetes_namespace.database.metadata[0].name
   timeout = 120
 
