fef177d0bae87c07f9fb0971579c5f9cee823067
Tiago Angelo <kurtis.angelo@gmail.com>
2021-03-21 15:45:44 -0300

    Fix Istio Ingress port mapping

--- kind-istio/istio-operator.tf
+++ kind-istio/istio-operator.tf
@@ -28,9 +28,26 @@ resource "kustomization_resource" "istio" {
         "defaultConfig" = { "tracing" = { "zipkin" = { "address" = "zipkin:9411" }, "sampling" = 100 } }
       }
       "components" = {
-        "egressGateways" = [ { enabled = false, name = "istio-egressgateway" }, ]
+        "egressGateways" = [ { enabled = false, name = "istio-egressgateway" } ]
+        "ingressGateways" = [
+          {
+            name = "istio-ingressgateway"
+            enabled = true
+            k8s = {
+              "nodeSelector" = { "ingress-ready" = "true" }
+              "service" = {
+                "ports" = [
+                  { name = "status-port", nodePort = 30002, port = 15021, targetPort = 15021 },
+                  { name = "http2", nodePort = 30000, port = 80, targetPort = 8080 },
+                  { name = "https", nodePort = 30001, port = 443, targetPort = 8443 },
+                ]
+              }
+            }
+          },
+        ]
       }
       "values" = {
+        "gateways" = { "istio-ingressgateway" = { "type" = "NodePort" } }
         "global" = {
           "defaultPodDisruptionBudget" = { "enabled" = false }
           "logging" = { "level" = "default:debug" }
@@ -43,8 +60,4 @@ resource "kustomization_resource" "istio" {
   depends_on = [
     helm_release.istio_operator
   ]
-}
-
-output "istio" {
-  value = yamldecode(file("istio.yaml"))
 }
