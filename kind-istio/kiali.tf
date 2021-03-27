06f8d09c36acbe93b136e2cfba95678d260d24a2
Tiago Angelo <kurtis.angelo@gmail.com>
2021-03-27 16:36:07 -0300

    Kiali installation

---
+++ kind-istio/kiali.tf
@@ -0,0 +1,63 @@
+resource "helm_release" "kiali" {
+  name = "kiali"
+  namespace = kubernetes_namespace.istio_system.metadata[0].name
+
+  repository = "https://kiali.org/helm-charts"
+  chart = "kiali-server"
+  version = "1.29.0"
+
+  set {
+    name = "nameOverride"
+    value = "kiali"
+  }
+
+  set {
+    name = "fullnameOverride"
+    value = "kiali"
+  }
+
+  values = [
+    yamlencode({
+      "auth" = { "strategy" = "anonymous" }
+      "deployment" = {
+        "accessible_namespaces" = [ "**" ]
+        "image_version" = "v1.29"
+        "ingress_enabled" = "false"
+        "pod_annotations" = { "sidecar.istio.io/inject" = "false" }
+      }
+      "login_token" = { "signing_key" = "CHANGEME" }
+    })
+  ]
+
+  depends_on = [
+    kustomization_resource.istio
+  ]
+}
+
+resource "kustomization_resource" "kiali_virtual_service" {
+  manifest = jsonencode({
+    "apiVersion" = "networking.istio.io/v1alpha3"
+    "kind" = "VirtualService"
+    "metadata" = {
+      "name" = "kiali-ui"
+      "namespace" = kubernetes_namespace.istio_system.metadata[0].name
+    }
+    "spec" = {
+      "gateways" = [ local.addons_gateway.name ]
+      "hosts" = [ local.kiali.host ]
+      "http" = [ {
+          route = [ {
+              destination = {
+                "host" = "kiali.${kubernetes_namespace.istio_system.metadata[0].name}.svc.cluster.local"
+                "port" = { "number" = 20001 }
+              }
+          } ]
+      } ]
+    }
+  })
+
+  depends_on = [
+    helm_release.kiali,
+    kustomization_resource.addons_gateway,
+  ]
+}
