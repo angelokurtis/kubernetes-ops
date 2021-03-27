d6c68f467f9a7055af6d6edf1907970875150e37
Tiago Angelo <kurtis.angelo@gmail.com>
2021-03-27 11:02:02 -0300

    Updating Istio version

--- kind-istio/istio-operator.tf
+++ kind-istio/istio-operator.tf
@@ -1,7 +1,12 @@
 resource "helm_release" "istio_operator" {
   name = "istio-operator"
-  chart = "https://s3-sa-east-1.amazonaws.com/charts.kurtis/istio-operator-1.9.1.tgz"
+  chart = "https://s3-sa-east-1.amazonaws.com/charts.kurtis/istio-operator-1.9.2.tgz"
   namespace = kubernetes_namespace.istio_system.metadata[0].name
+
+  set {
+    name = "tag"
+    value = "1.9.2-distroless"
+  }
 
   set {
     name = "operatorNamespace"
