5131736350363121b0cbd5e6177cf81808d642de
Tiago Angelo <kurtis.angelo@gmail.com>
2021-03-27 16:27:14 -0300

    Removing registry

--- kind-istio/variables.tf
+++ kind-istio/variables.tf
@@ -1,19 +1,3 @@
-variable "enable_local_registry" {
-  type = bool
-  description = "If set to true, it will create a local docker registry"
-  default = false
-}
-
-variable "registry_name" {
-  type = string
-  default = "kind-registry"
-}
-
-variable "registry_port" {
-  type = number
-  default = 5000
-}
-
 variable "ingress_domain" {
   type = string
   default = "istio.local"
