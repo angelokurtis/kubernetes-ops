be49c54e4889c0cb40ed2d760f743a82293a0cc3
Tiago Angelo <kurtis.angelo@gmail.com>
2021-05-11 15:41:33 -0300

    Renaming core component for its new version

--- kind-horusec/horusec-core.tf
+++ kind-horusec/horusec-core.tf
@@ -5,4 +5,8 @@ resource "helm_release" "core" {
   chart = "${var.horusec_project_path}/deployments/helm/core"
   namespace = kubernetes_namespace.horusec.metadata[0].name
   timeout = 240
+
+  depends_on = [
+    helm_release.auth
+  ]
 }
