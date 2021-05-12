2680b472bfcc7282033b5d3d20e27e5b44342567
Tiago Angelo <kurtis.angelo@gmail.com>
2021-05-12 20:45:02 -0300

    Install newest ArgoCD version with a default password set

--- kind-argo/argocd.tf
+++ kind-argo/argocd.tf
@@ -1,5 +1,5 @@
-resource "helm_release" "argo_cd" {
-  name = "argo-cd"
+resource "helm_release" "argocd" {
+  name = "argocd"
   namespace = kubernetes_namespace.ops.metadata[0].name
 
   repository = "https://argoproj.github.io/argo-helm"
@@ -8,11 +8,19 @@ resource "helm_release" "argo_cd" {
 
   values = [
     yamlencode({
+      "fullnameOverride" = "argocd",
+      "global" = { "image" = { "repository": "argoproj/argocd", "tag": "v2.0.1" } }
       "server" = {
         "extraArgs" = [ "--insecure" ]
         "ingress" = {
           "enabled" = true
-          "hosts" = [ "argo-cd.ops.local" ]
+          "hosts" = [ "argocd.local" ]
+        }
+      }
+      "configs" = {
+        "secret" = {
+          "argocdServerAdminPassword" = bcrypt("admin")
+          "argocdServerAdminPasswordMtime" = "2021-05-01T00:00:00Z"
         }
       }
     })
