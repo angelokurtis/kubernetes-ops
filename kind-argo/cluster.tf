866d053dfab0a09a13bb50cbb87efe74139eb01c
Tiago Angelo <kurtis.angelo@gmail.com>
2021-05-19 16:01:54 -0300

    Fix contour port

--- kind-argo/cluster.tf
+++ kind-argo/cluster.tf
@@ -8,7 +8,7 @@ resource "kind_cluster" "argo" {
 
     node {
       role = "control-plane"
-      image = "kindest/node:v1.18.15"
+      image = "kindest/node:v1.18.19"
 
       kubeadm_config_patches = [
         yamlencode({
@@ -18,13 +18,13 @@ resource "kind_cluster" "argo" {
       ]
 
       extra_port_mappings {
-        container_port = 80
+        container_port = 30080
         host_port = 80
         protocol = "TCP"
       }
 
       extra_port_mappings {
-        container_port = 443
+        container_port = 30443
         host_port = 443
         protocol = "TCP"
       }
