c693251e216eb7f6d76a7562004a0ca467648c67
Tiago Angelo <kurtis.angelo@gmail.com>
2021-07-24 19:51:35 -0300

    Using Let's Encrypt as certificate authority

--- kind-digitalocean/cert-manager.tf
+++ kind-digitalocean/cert-manager.tf
@@ -9,6 +9,24 @@ resource "helm_release" "cert_manager" {
   values = [ yamlencode({ "installCRDs" = true }) ]
 }
 
+resource "kustomization_resource" "letsencrypt_prod" {
+  manifest = jsonencode({
+    apiVersion = "cert-manager.io/v1"
+    kind = "ClusterIssuer"
+    metadata = { name = "letsencrypt" }
+    spec = {
+      acme = {
+        email = var.email
+        privateKeySecretRef = { name = "letsencrypt" }
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        solvers = [ { http01 = { ingress = { class = "nginx" } } } ]
+      }
+    }
+  })
+
+  depends_on = [ helm_release.cert_manager ]
+}
+
 resource "kubernetes_namespace" "cert_manager" {
   metadata { name = "cert-manager" }
 }
