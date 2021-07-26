436233eb196bca82ea53f37540c28cb5c5848c9e
Tiago Angelo <kurtis.angelo@gmail.com>
2021-07-26 18:06:58 -0300

    Host to be resolved by nip.io

--- do-workflows/ingress-controller.tf
+++ do-workflows/ingress-controller.tf
@@ -1,3 +1,8 @@
+locals {
+  ingress_ip = data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].ip
+  cluster_host = "${join("", formatlist("%x", split(".", local.ingress_ip)))}.nip.io"
+}
+
 resource "helm_release" "nginx" {
   name = "nginx"
   namespace = kubernetes_namespace.ingress_controller.metadata[0].name
@@ -5,6 +10,20 @@ resource "helm_release" "nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
   version = "3.34.0"
+
+  set {
+    name = "fullnameOverride"
+    value = "nginx"
+  }
+}
+
+data "kubernetes_service" "ingress_nginx" {
+  metadata {
+    name = "nginx-controller"
+    namespace = kubernetes_namespace.ingress_controller.metadata[0].name
+  }
+
+  depends_on = [ helm_release.nginx ]
 }
 
 resource "kubernetes_namespace" "ingress_controller" {
