17a7b2ba4e12c236048f75ff0fa212da9dcf606f
Tiago Angelo <kurtis.angelo@gmail.com>
2021-07-26 18:19:34 -0300

    Prepare for additional applications

--- do-workflows/argo-cd.tf
+++ do-workflows/argo-cd.tf
@@ -8,16 +8,18 @@ resource "helm_release" "argo_cd" {
 
   values = [
     yamlencode({
-      "fullnameOverride" = "argo-cd",
-      "global" = { "image" = { "repository": "argoproj/argocd", "tag": "v2.0.5" } }
-      "server" = {
-        "ingress" = { "enabled" = true, "hosts" = [ "argo-cd-${local.cluster_host}" ] }
-      }
-      "configs" = {
-        "secret" = {
-          "argocdServerAdminPassword" = "$2a$10$XgehbPznARhg75GhRgNNee/N/0bKsUWmdvvvuJ.W6JyNO/SGzuoRa",
-          "argocdServerAdminPasswordMtime" = "2021-07-01T00:00:00Z"
+      fullnameOverride = "argo-cd",
+      global = { image = { repository: "argoproj/argocd", tag: "v2.0.5" } }
+      configs = {
+        secret = {
+          argocdServerAdminPassword = "$2a$10$XgehbPznARhg75GhRgNNee/N/0bKsUWmdvvvuJ.W6JyNO/SGzuoRa",
+          argocdServerAdminPasswordMtime = "2021-07-01T00:00:00Z"
         }
+      }
+      server = {
+        ingress = { enabled = true, hosts = [ "argo-cd-${local.cluster_host}" ] }
+        additionalApplications = [
+        ]
       }
     })
   ]
