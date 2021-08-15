64e97b85d9e82c216cbcd5983dcfe991f028f24c
Tiago Angelo <kurtis.angelo@gmail.com>
2021-08-15 11:26:47 -0300

    Kind cluster for RBAC tests

---
+++ kind-rbac/ingress-nginx.tf
@@ -0,0 +1,27 @@
+resource "helm_release" "ingress_nginx" {
+  name = "ingress-nginx"
+  namespace = kubernetes_namespace.ingress.metadata[0].name
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
+  version = "3.35.0"
+
+  values = [
+    yamlencode({
+      "controller" = {
+        "extraArgs" = { "publish-status-address": "127.0.0.1" }
+        "publishService" = { "enabled" = false }
+        "service" = { "type" = "NodePort" }
+        "hostPort" = {
+          "enabled" = true
+          "ports" = { "http" = 80, "https" = 443 }
+        }
+        "nodeSelector" = { "ingress-ready" = "true", "kubernetes.io/os" = "linux" }
+      }
+    })
+  ]
+}
+
+resource "kubernetes_namespace" "ingress" {
+  metadata { name = "ingress" }
+}
