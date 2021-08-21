2a7ca1cf82021ee08ea582d559a786fe3b49e447
Tiago Angelo <kurtis.angelo@gmail.com>
2021-08-21 17:23:56 -0300

    Process Namespace Sharing (pns) as Workflow Executor

--- kind-workflows/argo-workflows.tf
+++ kind-workflows/argo-workflows.tf
@@ -18,25 +18,27 @@ resource "helm_release" "argo_workflows" {
   values = [
     yamlencode({
       executor = { image = { tag = local.argo_workflows.version } }
-      server = { image = { tag = local.argo_workflows.version } }
       controller: {
         image = { tag = local.argo_workflows.version }
+        containerRuntimeExecutor = "pns"
         persistence: {
           archive = true
           archiveTTL = "7d"
           connectionPool = { connMaxLifetime = "0s", maxIdleConns = 100, maxOpenConns = 0 }
           nodeStatusOffLoad = true
           postgresql = {
-            database = "postgres"
             host = "postgresql.${kubernetes_namespace.database.metadata[0].name}.svc.cluster.local"
-            passwordSecret = { key = "password", name = kubernetes_secret.argo_postgres_config.metadata[0].name }
             port = 5432
+            database = "postgres"
             tableName = "argo_workflows"
             userNameSecret = { key = "username", name = kubernetes_secret.argo_postgres_config.metadata[0].name }
+            passwordSecret = { key = "password", name = kubernetes_secret.argo_postgres_config.metadata[0].name }
           }
         }
       }
       server = {
+        image = { tag = local.argo_workflows.version }
+        extraArgs = [ "--auth-mode", "server" ]
         ingress = { enabled = true, hosts = [ "argo-workflows.lvh.me" ] }
       }
     })
