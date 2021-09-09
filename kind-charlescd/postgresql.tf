89ac37e81f0eff83d3dc6a90d26a9d06626c32af
Tiago Angelo <kurtis.angelo@gmail.com>
2021-09-09 17:21:56 -0300

    Configure databases

--- kind-charlescd/postgresql.tf
+++ kind-charlescd/postgresql.tf
@@ -1,34 +1,63 @@
-//locals {
-//  charlescd = { version = "1.0.1" }
-//}
-//
-//resource "helm_release" "charlescd" {
-//  name = "charlescd"
-//  namespace = kubernetes_namespace.cd.metadata[0].name
-//
-//  repository = "https://charts.kurtis.dev.br/"
-//  chart = "charlescd"
-//  version = "0.7.0"
-//
-//  values = [
-//    yamlencode({
-//      hostGlobal = "http://charles.${local.cluster_domain}"
-//      CharlesApplications = {
-//        butler = { image = { tag = local.charlescd.version } }
-//        circleMatcher = { image = { tag = local.charlescd.version } }
-//        compass = { image = { tag = local.charlescd.version } }
-//        gate = { image = { tag = local.charlescd.version } }
-//        moove = { image = { tag = local.charlescd.version } }
-//        ui = { image = { tag = local.charlescd.version } }
-//        villager = { image = { tag = local.charlescd.version } }
-//      }
-//      ingress = { enabled = false }
-//      envoy = { enabled = false }
-//      postgresql = { enabled = false }
-//      redis = { enabled = false }
-//      keycloak = { enabled = false }
-//      nginx_ingress_controller = { enabled = false }
-//      rabbitmq = { enabled = false }
-//    })
-//  ]
-//}
+locals {
+  databases = [
+    "charlescd_moove",
+    "charlescd_villager",
+    "charlescd_butler",
+    "charlescd_hermes",
+    "charlescd_compass",
+  ]
+  database = {for db in local.databases : db => {
+    database = "${db}_db"
+    user = db
+    password = random_password.databases[db].result
+  }}
+}
+
+resource "random_password" "databases" {
+  for_each = toset(local.databases)
+  keepers = { database = each.key }
+  length = 16
+  special = false
+}
+
+resource "helm_release" "postgresql" {
+  name = "postgresql"
+  namespace = kubernetes_namespace.database.metadata[0].name
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart = "postgresql"
+  version = "10.9.5"
+
+  set {
+    name = "initdbScriptsSecret"
+    value = kubernetes_secret.userdata.metadata[0].name
+  }
+
+  set {
+    name = "fullnameOverride"
+    value = "postgresql"
+  }
+}
+
+resource "kubernetes_namespace" "database" {
+  metadata {
+    name = "database"
+  }
+}
+
+resource "kubernetes_secret" "userdata" {
+  metadata {
+    name = "userdata"
+    namespace = kubernetes_namespace.database.metadata[0].name
+  }
+  data = {
+    "userdata.sql" = <<-EOT
+      %{ for db in local.databases ~}
+      create database ${local.database[db]["database"]};
+      create user ${local.database[db]["user"]} with encrypted password '${local.database[db]["password"]}';
+      alter user ${local.database[db]["user"]} with superuser;
+      grant all privileges on database ${local.database[db]["database"]} to ${local.database[db]["user"]};
+      %{ endfor ~}
+    EOT
+  }
+}
