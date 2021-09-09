d9a640078331913a03738c43aabf166d53e7a63b
Tiago Angelo <kurtis.angelo@gmail.com>
2021-09-09 10:45:56 -0300

    Install latest Istio version

---
+++ kind-charlescd/istio-operator.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "istio_operator" {
+  name = "istio-operator"
+  namespace = kubernetes_namespace.istio_system.metadata[0].name
+
+  repository = "https://charts.kurtis.dev.br/"
+  chart = "istio-operator"
+  version = "1.11.2"
+
+  set {
+    name = "operatorNamespace"
+    value = "istio-operator"
+  }
+
+  set {
+    name = "watchedNamespaces"
+    value = kubernetes_namespace.istio_system.metadata[0].name
+  }
+}
---
+++ kind-charlescd/istio.tf
@@ -0,0 +1,44 @@
+resource "kustomization_resource" "istio" {
+  manifest = jsonencode({
+    "apiVersion" = "install.istio.io/v1alpha1"
+    "kind" = "IstioOperator"
+    "metadata" = {
+      "name" = "control-plane"
+      "namespace" = kubernetes_namespace.istio_system.metadata[0].name
+    }
+    "spec" = {
+      "profile" = "demo"
+      "components" = {
+        "egressGateways" = [ { enabled = false, name = "istio-egressgateway" } ]
+        "ingressGateways" = [
+          {
+            name = "istio-ingressgateway"
+            enabled = true
+            k8s = {
+              "nodeSelector" = { "ingress-ready" = "true" }
+              "service" = {
+                "ports" = [
+                  { name = "status-port", nodePort = 30002, port = 15021, targetPort = 15021 },
+                  { name = "http2", nodePort = 30000, port = 80, targetPort = 8080 },
+                  { name = "https", nodePort = 30001, port = 443, targetPort = 8443 },
+                ]
+              }
+            }
+          },
+        ]
+      }
+      "values" = {
+        "gateways" = { "istio-ingressgateway" = { "type" = "NodePort" } }
+        "global" = {
+          "defaultPodDisruptionBudget" = { "enabled" = false }
+          "logging" = { "level" = "default:debug" }
+          "proxy" = { "componentLogLevel" = "misc:debug", "logLevel" = "debug" }
+        }
+      }
+    }
+  })
+
+  depends_on = [
+    helm_release.istio_operator
+  ]
+}
