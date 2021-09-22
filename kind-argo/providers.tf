4ba303fde49f43849a08ba208166e242bad5974f
Tiago Angelo <kurtis.angelo@gmail.com>
2021-09-21 22:17:20 -0300

    Updating providers

--- kind-argo/providers.tf
+++ kind-argo/providers.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
     kind = {
-      source = "kyma-incubator/kind"
-      version = "0.0.7"
-    }
-    kustomization = {
-      source = "kbst/kustomization"
-      version = "0.5.0"
+      source  = "kyma-incubator/kind"
+      version = "0.0.9"
     }
   }
   required_version = ">= 0.13"
@@ -22,8 +18,4 @@ provider "kind" {}
 
 provider "kubernetes" {
   config_path = kind_cluster.argo.kubeconfig_path
-}
-
-provider "kustomization" {
-  kubeconfig_path = kind_cluster.argo.kubeconfig_path
 }
