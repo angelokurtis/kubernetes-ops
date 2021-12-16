6b436576e1508a647f91e68d77a614ed6a71f5c1
Tiago Angelo <kurtis.angelo@gmail.com>
2021-12-16 14:21:52 -0300

    Install Flux2

---
+++ kind-fluxcd/flux.tf
@@ -0,0 +1,21 @@
+locals {
+  flux = { version = "v0.24.1" }
+}
+
+data "kustomization_overlay" "flux" {
+  resources = [
+    "https://github.com/fluxcd/flux2/manifests/bases/source-controller?ref=${local.flux.version}",
+    "https://github.com/fluxcd/flux2/manifests/bases/kustomize-controller?ref=${local.flux.version}",
+    "https://github.com/fluxcd/flux2/manifests/bases/helm-controller?ref=${local.flux.version}",
+    "https://github.com/fluxcd/flux2/manifests/rbac?ref=${local.flux.version}",
+    "https://github.com/fluxcd/flux2/manifests/policies?ref=${local.flux.version}",
+  ]
+  namespace = var.flux_namespace
+}
+
+resource "kustomization_resource" "flux" {
+  for_each = data.kustomization_overlay.flux.ids
+  manifest = data.kustomization_overlay.flux.manifests[each.value]
+
+  depends_on = [kubernetes_namespace.flux]
+}
