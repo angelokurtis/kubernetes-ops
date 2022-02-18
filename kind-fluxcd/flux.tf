218ea903545037d5a58a5bd6fac8e8474ee77f97
Tiago Angelo <kurtis.angelo@gmail.com>
2022-02-18 11:24:12 -0300

    Update ClusterRoleBinding with defined namespace

--- kind-fluxcd/flux.tf
+++ kind-fluxcd/flux.tf
@@ -11,6 +11,18 @@ data "kustomization_overlay" "flux" {
     "https://github.com/fluxcd/flux2/manifests/policies?ref=${local.flux.version}",
   ]
   namespace = var.flux_namespace
+  patches {
+    target = { apiVersion = "rbac.authorization.k8s.io/v1", kind = "ClusterRoleBinding", name = "cluster-reconciler" }
+    patch  = yamlencode([
+    for i in range(2) : { op = "replace", path = "/subjects/${i}/namespace", value = var.flux_namespace }
+    ])
+  }
+  patches {
+    target = { apiVersion = "rbac.authorization.k8s.io/v1", kind = "ClusterRoleBinding", name = "crd-controller" }
+    patch  = yamlencode([
+    for i in range(6) : { op = "replace", path = "/subjects/${i}/namespace", value = var.flux_namespace }
+    ])
+  }
 }
 
 resource "kustomization_resource" "flux" {
@@ -19,3 +31,10 @@ resource "kustomization_resource" "flux" {
 
   depends_on = [kubernetes_namespace.flux]
 }
+
+# used for troubleshooting
+# resource "local_file" "flux" {
+#   for_each = data.kustomization_overlay.flux.ids
+#   content  = yamlencode(jsondecode(data.kustomization_overlay.flux.manifests[each.value]))
+#   filename = "${path.module}/flux/${each.value}.yaml"
+# }
