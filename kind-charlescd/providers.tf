778e9cda213c5ea05c5878b47291ad8ffd4da581
Tiago Angelo <kurtis.angelo@gmail.com>
2022-02-23 15:35:33 -0300

    Update providers

--- kind-charlescd/providers.tf
+++ kind-charlescd/providers.tf
@@ -1,37 +1,30 @@
 terraform {
   required_providers {
-    kind          = {
-      source  = "kyma-incubator/kind"
-      version = ">= 0.0"
-    }
-    kustomization = {
-      source  = "kbst/kustomization"
-      version = ">= 0.5"
-    }
-    helm          = {
-      source  = "hashicorp/helm"
-      version = ">= 2.3"
-    }
-    kubernetes    = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.5"
-    }
+    kind       = { source = "kyma-incubator/kind", version = ">= 0.0" }
+    flux       = { source = "fluxcd/flux", version = ">= 0.11" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.8" }
+    kubectl    = { source = "gavinbunney/kubectl", version = ">= 1.13" }
   }
   required_version = ">= 1.0"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = kind_cluster.charlescd.kubeconfig_path
-  }
-}
-
 provider "kind" {}
 
+provider "flux" {}
+
 provider "kubernetes" {
-  config_path = kind_cluster.charlescd.kubeconfig_path
+  host = kind_cluster.charlescd.endpoint
+
+  client_certificate     = kind_cluster.charlescd.client_certificate
+  client_key             = kind_cluster.charlescd.client_key
+  cluster_ca_certificate = kind_cluster.charlescd.cluster_ca_certificate
 }
 
-provider "kustomization" {
-  kubeconfig_path = kind_cluster.charlescd.kubeconfig_path
+provider "kubectl" {
+  host = kind_cluster.charlescd.endpoint
+
+  client_certificate     = kind_cluster.charlescd.client_certificate
+  client_key             = kind_cluster.charlescd.client_key
+  cluster_ca_certificate = kind_cluster.charlescd.cluster_ca_certificate
+  load_config_file       = false
 }
