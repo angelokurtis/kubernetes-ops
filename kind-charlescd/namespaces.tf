06b120b6d367e84583897b6329081f29fe0a5755
Tiago Angelo <kurtis.angelo@gmail.com>
2022-02-23 16:17:43 -0300

    Install Redis 6.2

--- kind-charlescd/namespaces.tf
+++ kind-charlescd/namespaces.tf
@@ -1,7 +1,3 @@
 resource "kubernetes_namespace" "continuous_deployment" {
   metadata { name = "continuous-deployment" }
 }
-
-resource "kubernetes_namespace" "cache" {
-  metadata { name = "cache" }
-}
--- kind-charlescd/redis.tf
+++ kind-charlescd/redis.tf
@@ -1,35 +1,31 @@
-resource "helm_release" "redis" {
-  name      = "redis"
-  namespace = kubernetes_namespace.cache.metadata[0].name
+resource "kubectl_manifest" "redis_helm_release" {
+  yaml_body = <<YAML
+apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: redis
+  namespace: ${kubernetes_namespace.redis.metadata[0].name}
+spec:
+  interval: ${local.flux.default_interval}
+  chart:
+    spec:
+      chart: redis
+      sourceRef:
+        kind: HelmRepository
+        name: bitnami
+        namespace: default
+  values:
+    nameOverride: redis
+    architecture: standalone
+    auth:
+      existingSecret: "${kubernetes_secret.redis.metadata[0].name}"
+      existingSecretPasswordKey: password
+YAML
 
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "redis"
-  version    = "15.3.2"
-
-  set {
-    name  = "nameOverride"
-    value = "redis"
-  }
-
-  set {
-    name  = "architecture"
-    value = "standalone"
-  }
-
-  set {
-    name  = "auth.existingSecret"
-    value = kubernetes_secret.redis.metadata[0].name
-  }
-
-  set {
-    name  = "auth.existingSecretPasswordKey"
-    value = "password"
-  }
-
-  set {
-    name  = "image.tag"
-    value = "6.2"
-  }
+  depends_on = [
+    kubectl_manifest.fluxcd,
+    kubectl_manifest.bitnami_helm_repository
+  ]
 }
 
 resource "random_password" "redis" {
@@ -40,9 +36,13 @@ resource "random_password" "redis" {
 resource "kubernetes_secret" "redis" {
   metadata {
     name      = "redis"
-    namespace = kubernetes_namespace.cache.metadata[0].name
+    namespace = kubernetes_namespace.redis.metadata[0].name
   }
   data = {
     password = random_password.redis.result
   }
+}
+
+resource "kubernetes_namespace" "redis" {
+  metadata { name = var.redis_namespace }
 }
