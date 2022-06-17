8bd43653b16528db84c35f6f33c4f8f024809308
Tiago Angelo <kurtis.angelo@gmail.com>
2022-06-17 14:21:48 -0300

    Wait for Jaeger CRDs

--- kind-fluxcd/jaeger.tf
+++ kind-fluxcd/jaeger.tf
@@ -59,12 +59,13 @@ spec:
     type: memory
   strategy: allinone
   allInOne:
-    image: jaegertracing/all-in-one:1.35.1
+    image: jaegertracing/all-in-one:1.35.2
 YAML
 
   depends_on = [
     kubectl_manifest.fluxcd,
-    kubectl_manifest.jaeger_operator_helm_release
+    kubectl_manifest.jaeger_operator_helm_release,
+    kubernetes_job_v1.wait_jaeger_crds
   ]
 }
 
@@ -90,6 +91,78 @@ spec:
 YAML
 
   depends_on = [kubectl_manifest.jaeger]
+}
+
+resource "kubernetes_job_v1" "wait_jaeger_crds" {
+  metadata {
+    name      = "wait-jaeger-crds"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        service_account_name = kubernetes_service_account_v1.jaeger_kubectl.metadata[0].name
+        container {
+          name  = "kubectl"
+          image = "docker.io/bitnami/kubectl:${data.kubectl_server_version.current.major}.${data.kubectl_server_version.current.minor}"
+          args  = ["wait", "--for=condition=Ready", "helmrelease/jaeger-operator", "--timeout", local.default_timeouts]
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+  wait_for_completion = true
+
+  timeouts {
+    create = local.default_timeouts
+    update = local.default_timeouts
+  }
+
+  depends_on = [
+    kubernetes_role_binding_v1.kubectl_jaeger_helmreleases_reader,
+    kubectl_manifest.jaeger_operator_helm_release,
+  ]
+}
+
+data "kubectl_server_version" "current" {
+  depends_on = [kind_cluster.flux]
+}
+
+resource "kubernetes_service_account_v1" "jaeger_kubectl" {
+  metadata {
+    name      = "kubectl"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+}
+
+resource "kubernetes_role_v1" "jaeger_helmreleases_reader" {
+  metadata {
+    name      = "jaeger-helmreleases-reader"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+  rule {
+    api_groups = ["helm.toolkit.fluxcd.io"]
+    resources  = ["helmreleases"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_role_binding_v1" "kubectl_jaeger_helmreleases_reader" {
+  metadata {
+    name      = "kubectl-jaeger-helmreleases-reader"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role_v1.jaeger_helmreleases_reader.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account_v1.jaeger_kubectl.metadata[0].name
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
 }
 
 resource "kubernetes_namespace" "jaeger" {
