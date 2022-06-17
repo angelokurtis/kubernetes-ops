266f193eb8955c8a1e67a540378211a7c343ffb3
Tiago Angelo <kurtis.angelo@gmail.com>
2022-06-17 14:22:49 -0300

    Run LocalStack mocking the S3 bucket

--- kind-fluxcd/providers.tf
+++ kind-fluxcd/providers.tf
@@ -1,5 +1,7 @@
 terraform {
   required_providers {
+    aws        = { source = "hashicorp/aws", version = ">= 4.18.0, < 4.19.0" }
+    docker     = { source = "kreuzwerker/docker", version = ">= 2.16.0, < 2.17.0" }
     kind       = { source = "tehcyx/kind", version = ">= 0.0.12, < 0.1.0" }
     flux       = { source = "fluxcd/flux", version = ">= 0.15.1, < 0.16.0" }
     kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.11.0, < 2.12.0" }
@@ -7,6 +9,40 @@ terraform {
   }
   required_version = ">= 1.0"
 }
+
+provider "aws" {
+  access_key                  = "m0(k_4((355_k3y"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  secret_key                  = "m0(k_53(r37_k3y"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
+
+provider "docker" {}
 
 provider "kind" {}
 
