cfa04304bf53dcaac69bcfb93a546d9d547c527f
Tiago Angelo <kurtis.angelo@gmail.com>
2022-09-08 18:28:46 -0300

    Apply OpenTelemetryCollector

---
+++ kind-otel/opentelemetry-collector.tf
@@ -0,0 +1,27 @@
+resource "kubectl_manifest" "otelcol" {
+  server_side_apply = true
+  yaml_body         = yamlencode({
+    apiVersion = "opentelemetry.io/v1alpha1"
+    kind       = "OpenTelemetryCollector"
+    metadata   = { name = "default", namespace = kubernetes_namespace_v1.opentelemetry.metadata[0].name }
+    spec       = {
+      image    = "docker.io/kurtis/otel-collector:v1.0.5"
+      mode     = "statefulset"
+      replicas = 1
+      ports    = [{ name = "prometheus", port = 8889, targetPort = 8889 }]
+      env      = [
+        {
+          name  = "JAEGER_OTLP_ENDPOINT",
+          value = "jaeger-collector.${kubernetes_namespace_v1.jaeger.metadata[0].name}.svc.cluster.local:4317"
+        },
+        {
+          name  = "PROMETHEUS_PUSHGATEWAY_ENDPOINT",
+          value = "prometheus-server.${kubernetes_namespace_v1.prometheus.metadata[0].name}.svc.cluster.local:80"
+        }
+      ]
+      config = file("opentelemetry-collector.yaml")
+    }
+  })
+
+  depends_on = [kubectl_manifest.fluxcd, kubernetes_job_v1.wait_helm_release["opentelemetry-operator"]]
+}
