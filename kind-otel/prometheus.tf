1436f51ebf37166636703854640a22280e5cfffa
Tiago Angelo <kurtis.angelo@gmail.com>
2022-09-14 09:57:05 -0300

    Enabling verticalpodautoscalers metrics

--- kind-otel/prometheus.tf
+++ kind-otel/prometheus.tf
@@ -1,10 +1,42 @@
 locals {
   prometheus = {
-    nodeExporter     = { enabled = false }
-    kubeStateMetrics = { enabled = false }
-    pushgateway      = { enabled = false }
-    alertmanager     = { enabled = false }
-    configmapReload  = {
+    nodeExporter       = { enabled = true }
+    kubeStateMetrics   = { enabled = true }
+    kube-state-metrics = {
+      collectors = [
+        "certificatesigningrequests",
+        "configmaps",
+        "cronjobs",
+        "daemonsets",
+        "deployments",
+        "endpoints",
+        "horizontalpodautoscalers",
+        "ingresses",
+        "jobs",
+        "limitranges",
+        "mutatingwebhookconfigurations",
+        "namespaces",
+        "networkpolicies",
+        "nodes",
+        "persistentvolumeclaims",
+        "persistentvolumes",
+        "poddisruptionbudgets",
+        "pods",
+        "replicasets",
+        "replicationcontrollers",
+        "resourcequotas",
+        "secrets",
+        "services",
+        "statefulsets",
+        "storageclasses",
+        "validatingwebhookconfigurations",
+        "volumeattachments",
+        "verticalpodautoscalers",
+      ]
+    }
+    pushgateway     = { enabled = false }
+    alertmanager    = { enabled = false }
+    configmapReload = {
       prometheus = { image = { repository = "jimmidyson/configmap-reload", tag = "v0.7.1" } }
     }
     server = {
--- kind-otel/vertical-pod-autoscaler.tf
+++ kind-otel/vertical-pod-autoscaler.tf
@@ -1,12 +1,10 @@
 locals {
   vertical_pod_autoscaler = {
-    admissionController = {
-      extraArgs = { v = "2", vpa-object-namespace = kubernetes_namespace_v1.opentelemetry.metadata[0].name }
-    }
     recommender = {
       extraArgs = { v = "2", vpa-object-namespace = kubernetes_namespace_v1.opentelemetry.metadata[0].name }
     }
-    updater = { enabled = false }
+    admissionController = { enabled = false }
+    updater             = { enabled = false }
   }
 }
 
