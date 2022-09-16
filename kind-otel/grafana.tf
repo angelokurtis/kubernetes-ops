b47094771dba26a21b0e53bab0a2de159f8f3ce5
Tiago Angelo <kurtis.angelo@gmail.com>
2022-09-16 15:48:04 -0300

    Set up predefined dashboards

--- kind-otel/grafana.tf
+++ kind-otel/grafana.tf
@@ -1,15 +1,13 @@
 locals {
   grafana = {
-    image = {
-      repository = "grafana/grafana"
-      tag        = "9.1.5"
-    }
+    image         = { repository = "grafana/grafana", tag = "9.1.5" }
     ingress       = { enabled = true, hosts = ["grafana.${local.cluster_host}"], ingressClassName = "nginx" }
+    admin         = { existingSecret = kubernetes_secret_v1.grafana_credentials.metadata[0].name }
     "grafana.ini" = {
       "auth.anonymous" = {
         enabled  = true
         org_name = "Main Org."
-        org_role = "Admin"
+        org_role = "Viewer"
       }
     }
     datasources = {
@@ -26,11 +24,44 @@ locals {
         ]
       }
     }
-    dashboards = {
-      default = {
-        vpa-recommendations = { gnetId = 14588, datasource = "prometheus" }
+    dashboardProviders = {
+      "dashboardproviders.yaml" = {
+        apiVersion = 1
+        providers  = [
+          {
+            disableDeletion = false
+            editable        = true
+            folder          = ""
+            name            = "default"
+            options         = { path = "/var/lib/grafana/dashboards/default" }
+            orgId           = 1
+            type            = "file"
+          },
+        ]
       }
     }
+    dashboardsConfigMaps = { default = kubernetes_config_map_v1.grafana_dashboards.metadata[0].name }
+  }
+}
+
+resource "kubernetes_secret_v1" "grafana_credentials" {
+  metadata {
+    name      = "grafana-credentials"
+    namespace = kubernetes_namespace_v1.grafana.metadata[0].name
+  }
+  data = {
+    admin-user     = "admin"
+    admin-password = "admin"
+  }
+}
+
+resource "kubernetes_config_map_v1" "grafana_dashboards" {
+  metadata {
+    name      = "grafana-dashboards"
+    namespace = kubernetes_namespace_v1.grafana.metadata[0].name
+  }
+  binary_data = {
+    "vpa-recommendations.json" = filebase64("dashboards/vpa-recommendations.json")
   }
 }
 
