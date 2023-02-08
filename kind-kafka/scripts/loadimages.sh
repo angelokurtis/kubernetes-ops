7a4acda56a20122c1586f12960c0254d7b2bbfe7
Tiago Angelo <kurtis.angelo@gmail.com>
2023-02-08 14:39:02 -0300

    Update images

--- kind-kafka/scripts/loadimages.sh
+++ kind-kafka/scripts/loadimages.sh
@@ -16,10 +16,9 @@ loadImage ghcr.io/fluxcd/helm-controller:v0.29.0
 loadImage ghcr.io/fluxcd/kustomize-controller:v0.33.0
 loadImage ghcr.io/fluxcd/source-controller:v0.34.0
 loadImage jimmidyson/configmap-reload:v0.8.0
-loadImage prom/pushgateway:v1.5.1
 loadImage quay.io/jcmoraisjr/haproxy-ingress:v0.14.0
 loadImage quay.io/prometheus/node-exporter:v1.5.0
-loadImage quay.io/prometheus/prometheus:v2.40.7
+loadImage quay.io/prometheus/prometheus:v2.42.0
 loadImage quay.io/strimzi/kafka:0.33.1-kafka-3.3.2
 loadImage quay.io/strimzi/operator:0.33.1
 loadImage registry.k8s.io/kube-state-metrics/kube-state-metrics:v2.7.0
