91c56b0f01661040c8d35bcd257f8cb8ea545d41
Tiago Angelo <kurtis.angelo@gmail.com>
2024-01-04 17:27:52 -0300

    Add cert-manager resources to Kubernetes cluster

--- kind-otel/cert-manager.tf
+++ kind-otel/cert-manager.tf
@@ -1,10 +1,56 @@
-locals {
-  cert_manager = {
-    installCRDs = true
-    prometheus  = { enabled = false }
+resource "kubectl_manifest" "helm_repository_jetstack" {
+  yaml_body = <<-YAML
+    apiVersion: source.toolkit.fluxcd.io/v1beta2
+    kind: HelmRepository
+    metadata:
+      name: jetstack
+      namespace: ${kubernetes_namespace.flux.metadata[0].name}
+    spec:
+      interval: 60s
+      url: https://charts.jetstack.io
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_flux_crd]
+}
+
+resource "kubectl_manifest" "helm_release_cert_manager" {
+  yaml_body = <<-YAML
+    apiVersion: helm.toolkit.fluxcd.io/v2beta1
+    kind: HelmRelease
+    metadata:
+      name: cert-manager
+      namespace: ${kubernetes_namespace.cert_manager.metadata[0].name}
+    spec:
+      chart:
+        spec:
+          chart: cert-manager
+          reconcileStrategy: ChartVersion
+          sourceRef:
+            kind: HelmRepository
+            name: jetstack
+            namespace: ${kubernetes_namespace.flux.metadata[0].name}
+      valuesFrom:
+        - kind: ConfigMap
+          name: ${kubernetes_config_map_v1.cert_manager_helm_values.metadata[0].name}
+      interval: 60s
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_flux_crd]
+}
+
+resource "kubernetes_config_map_v1" "cert_manager_helm_values" {
+  metadata {
+    name      = "cert-manager-helm-values"
+    namespace = kubernetes_namespace.cert_manager.metadata[0].name
+  }
+  data = {
+    "values.yaml" = yamlencode({
+      installCRDs = true
+      prometheus  = { enabled = false }
+    })
   }
 }
 
-resource "kubernetes_namespace_v1" "cert_manager" {
+resource "kubernetes_namespace" "cert_manager" {
   metadata { name = "cert-manager" }
 }
