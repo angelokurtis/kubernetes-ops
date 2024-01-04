5583ae13695ae6efc98c819226588312b8527117
Tiago Angelo <kurtis.angelo@gmail.com>
2024-01-04 18:02:37 -0300

    Update Grafana configuration and dependencies

--- kind-otel/grafana.tf
+++ kind-otel/grafana.tf
@@ -1,53 +1,99 @@
-locals {
-  grafana = {
-    image         = { repository = "grafana/grafana", tag = "9.3.2" }
-    ingress       = { enabled = true, hosts = ["grafana.${local.cluster_host}"], ingressClassName = "nginx" }
-    admin         = { existingSecret = kubernetes_secret_v1.grafana_credentials.metadata[0].name }
-    "grafana.ini" = {
-      "auth.anonymous" = {
-        enabled  = true
-        org_name = "Main Org."
-        org_role = "Viewer"
+resource "kubectl_manifest" "helm_repository_grafana" {
+  yaml_body = <<-YAML
+    apiVersion: source.toolkit.fluxcd.io/v1beta2
+    kind: HelmRepository
+    metadata:
+      name: grafana
+      namespace: ${kubernetes_namespace.flux.metadata[0].name}
+    spec:
+      interval: 60s
+      url: https://grafana.github.io/helm-charts
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_flux_crd]
+}
+
+resource "kubectl_manifest" "helm_release_grafana" {
+  yaml_body = <<-YAML
+    apiVersion: helm.toolkit.fluxcd.io/v2beta1
+    kind: HelmRelease
+    metadata:
+      name: grafana
+      namespace: ${kubernetes_namespace.grafana.metadata[0].name}
+    spec:
+      chart:
+        spec:
+          chart: grafana
+          reconcileStrategy: ChartVersion
+          sourceRef:
+            kind: HelmRepository
+            name: grafana
+            namespace: ${kubernetes_namespace.flux.metadata[0].name}
+      valuesFrom:
+        - kind: ConfigMap
+          name: ${kubernetes_config_map_v1.grafana_helm_values.metadata[0].name}
+      interval: 60s
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_flux_crd]
+}
+
+resource "kubernetes_config_map_v1" "grafana_helm_values" {
+  metadata {
+    name      = "grafana-helm-values"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+  }
+  data = {
+    "values.yaml" = yamlencode({
+      image         = { repository = "grafana/grafana", tag = "10.2.3" }
+      ingress       = { enabled = true, hosts = ["grafana.${local.cluster_host}"], ingressClassName = "nginx" }
+      admin         = { existingSecret = kubernetes_secret_v1.grafana_credentials.metadata[0].name }
+      "grafana.ini" = {
+        "auth.anonymous" = {
+          enabled  = true
+          org_name = "Main Org."
+          org_role = "Viewer"
+        }
       }
-    }
-    datasources = {
-      "datasources.yaml" = {
-        apiVersion  = 1
-        datasources = [
-          {
-            name      = "Prometheus"
-            type      = "prometheus"
-            url       = "http://prometheus-server.${kubernetes_namespace_v1.prometheus.metadata[0].name}:80"
-            access    = "proxy"
-            isDefault = true
-          }
-        ]
+      datasources = {
+        "datasources.yaml" = {
+          apiVersion  = 1
+          datasources = [
+            {
+              name      = "Prometheus"
+              type      = "prometheus"
+              url       = "http://prometheus-server.${kubernetes_namespace.prometheus.metadata[0].name}:80"
+              access    = "proxy"
+              isDefault = true
+            }
+          ]
+        }
       }
-    }
-    dashboardProviders = {
-      "dashboardproviders.yaml" = {
-        apiVersion = 1
-        providers  = [
-          {
-            disableDeletion = false
-            editable        = true
-            folder          = ""
-            name            = "default"
-            options         = { path = "/var/lib/grafana/dashboards/default" }
-            orgId           = 1
-            type            = "file"
-          },
-        ]
+      dashboardProviders = {
+        "dashboardproviders.yaml" = {
+          apiVersion = 1
+          providers  = [
+            {
+              disableDeletion = false
+              editable        = true
+              folder          = ""
+              name            = "default"
+              options         = { path = "/var/lib/grafana/dashboards/default" }
+              orgId           = 1
+              type            = "file"
+            },
+          ]
+        }
       }
-    }
-    dashboardsConfigMaps = { default = kubernetes_config_map_v1.grafana_dashboards.metadata[0].name }
+      dashboardsConfigMaps = { default = kubernetes_config_map_v1.grafana_dashboards.metadata[0].name }
+    })
   }
 }
 
 resource "kubernetes_secret_v1" "grafana_credentials" {
   metadata {
     name      = "grafana-credentials"
-    namespace = kubernetes_namespace_v1.grafana.metadata[0].name
+    namespace = kubernetes_namespace.grafana.metadata[0].name
   }
   data = {
     admin-user     = "admin"
@@ -58,13 +104,13 @@ resource "kubernetes_secret_v1" "grafana_credentials" {
 resource "kubernetes_config_map_v1" "grafana_dashboards" {
   metadata {
     name      = "grafana-dashboards"
-    namespace = kubernetes_namespace_v1.grafana.metadata[0].name
+    namespace = kubernetes_namespace.grafana.metadata[0].name
   }
   binary_data = {
     "kubernetes-pod-metrics.json" = filebase64("dashboards/kubernetes-pod-metrics.json")
   }
 }
 
-resource "kubernetes_namespace_v1" "grafana" {
+resource "kubernetes_namespace" "grafana" {
   metadata { name = "grafana" }
 }
