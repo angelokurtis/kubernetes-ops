f845a54518f44a4242ece89d9beac53934d2fc7c
Tiago Angelo <kurtis.angelo@gmail.com>
2024-01-04 17:31:56 -0300

    Update kind and kubernetes provider versions

--- kind-otel/providers.tf
+++ kind-otel/providers.tf
@@ -1,16 +1,22 @@
 terraform {
   required_providers {
-    flux       = { source = "fluxcd/flux", version = ">= 0.22.3, < 1.0.0" }
-    helm       = { source = "hashicorp/helm", version = ">= 2.8.0, < 3.0.0" }
-    kind       = { source = "tehcyx/kind", version = ">= 0.0.16, < 0.1.0" }
-    kubectl    = { source = "gavinbunney/kubectl", version = ">= 1.14.0, < 2.0.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.16.1, < 3.0.0" }
-    local      = { source = "hashicorp/local", version = ">= 2.2.3, < 3.0.0" }
+    kind       = { source = "tehcyx/kind", version = ">= 0.2.1, < 0.3.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.25.1, < 4.0.0" }
+    helm       = { source = "hashicorp/helm", version = ">= 2.12.1, < 3.0.0" }
+    kubectl    = { source = "alekc/kubectl", version = ">= 2.0.4, < 3.0.0" }
   }
   required_version = ">= 1.0"
 }
 
-provider "flux" {}
+provider "kind" {}
+
+provider "kubernetes" {
+  host = kind_cluster.otel.endpoint
+
+  client_certificate     = kind_cluster.otel.client_certificate
+  client_key             = kind_cluster.otel.client_key
+  cluster_ca_certificate = kind_cluster.otel.cluster_ca_certificate
+}
 
 provider "helm" {
   kubernetes {
@@ -22,7 +28,6 @@ provider "helm" {
   }
 }
 
-provider "kind" {}
 
 provider "kubectl" {
   host = kind_cluster.otel.endpoint
@@ -31,12 +36,4 @@ provider "kubectl" {
   client_key             = kind_cluster.otel.client_key
   cluster_ca_certificate = kind_cluster.otel.cluster_ca_certificate
   load_config_file       = false
-}
-
-provider "kubernetes" {
-  host = kind_cluster.otel.endpoint
-
-  client_certificate     = kind_cluster.otel.client_certificate
-  client_key             = kind_cluster.otel.client_key
-  cluster_ca_certificate = kind_cluster.otel.cluster_ca_certificate
 }
