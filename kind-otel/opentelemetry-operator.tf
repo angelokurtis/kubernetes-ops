8db718262d504426858b30a88d03f48cf42bb8a0
Tiago Angelo <kurtis.angelo@gmail.com>
2024-01-04 17:28:50 -0300

    Add Helm repository and release for OpenTelemetry operator

--- kind-otel/opentelemetry-operator.tf
+++ kind-otel/opentelemetry-operator.tf
@@ -1,16 +1,40 @@
-locals {
-  opentelemetry_operator = {
-    manager = {
-      collectorImage = { repository = "otel/opentelemetry-collector-contrib", tag = "0.70.0" }
-    }
-  }
+resource "kubectl_manifest" "helm_repository_opentelemetry" {
+  yaml_body = <<-YAML
+    apiVersion: source.toolkit.fluxcd.io/v1beta2
+    kind: HelmRepository
+    metadata:
+      name: opentelemetry
+      namespace: ${kubernetes_namespace.flux.metadata[0].name}
+    spec:
+      interval: 60s
+      url: https://open-telemetry.github.io/opentelemetry-helm-charts
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_flux_crd]
 }
 
-resource "kubernetes_namespace_v1" "opentelemetry" {
-  metadata {
-    name   = "otel"
-    labels = {
-      "goldilocks.fairwinds.com/enabled" = true
-    }
-  }
+resource "kubectl_manifest" "helm_release_opentelemetry_operator" {
+  yaml_body = <<-YAML
+    apiVersion: helm.toolkit.fluxcd.io/v2beta1
+    kind: HelmRelease
+    metadata:
+      name: opentelemetry-operator
+      namespace: ${kubernetes_namespace.opentelemetry.metadata[0].name}
+    spec:
+      chart:
+        spec:
+          chart: opentelemetry-operator
+          reconcileStrategy: ChartVersion
+          sourceRef:
+            kind: HelmRepository
+            name: opentelemetry
+            namespace: ${kubernetes_namespace.flux.metadata[0].name}
+      interval: 60s
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_flux_crd]
+}
+
+resource "kubernetes_namespace" "opentelemetry" {
+  metadata { name = "otel" }
 }
