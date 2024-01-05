b8db59adb86aab68b672e1ec610461276c178ff7
Tiago Angelo <kurtis.angelo@gmail.com>
2024-01-05 16:38:25 -0300

    Update container images for kind-otel deployment

--- kind-otel/scripts/loadimages.sh
+++ kind-otel/scripts/loadimages.sh
@@ -10,22 +10,25 @@ loadImage(){
   (set -x; kind load docker-image --name otel $t --nodes otel-worker)
 }
 
-loadImage docker.io/bitnami/kubectl:1.24
-loadImage gcr.io/kubebuilder/kube-rbac-proxy:v0.13.0
-loadImage ghcr.io/fluxcd/helm-controller:v0.28.1
-loadImage ghcr.io/fluxcd/kustomize-controller:v0.32.0
-loadImage ghcr.io/fluxcd/source-controller:v0.33.0
-loadImage ghcr.io/open-telemetry/opentelemetry-operator/opentelemetry-operator:v0.67.0
-loadImage grafana/grafana:9.3.2
-loadImage jaegertracing/all-in-one:1.41.0
-loadImage jimmidyson/configmap-reload:v0.8.0
+loadImage docker.io/bitnami/kubectl:1.27
+loadImage docker.io/grafana/grafana:10.2.3
+loadImage ghcr.io/fluxcd/helm-controller:v0.37.2
+loadImage ghcr.io/fluxcd/image-automation-controller:v0.37.0
+loadImage ghcr.io/fluxcd/image-reflector-controller:v0.31.1
+loadImage ghcr.io/fluxcd/kustomize-controller:v1.2.1
+loadImage ghcr.io/fluxcd/notification-controller:v1.2.3
+loadImage ghcr.io/fluxcd/source-controller:v1.2.3
+loadImage ghcr.io/open-telemetry/opentelemetry-operator/opentelemetry-operator:v0.90.0
+loadImage ghcr.io/weaveworks/wego-app:v0.38.0
+loadImage jaegertracing/all-in-one:1.52.0
 loadImage kurtis/bets:1.0.0-java-spring
 loadImage kurtis/championships:1.0.0-java-spring
 loadImage kurtis/matches:1.0.0-java-spring
 loadImage kurtis/teams:1.0.0-java-spring
-loadImage otel/opentelemetry-collector-contrib:0.69.0
-loadImage quay.io/jetstack/cert-manager-cainjector:v1.11.0
-loadImage quay.io/jetstack/cert-manager-controller:v1.11.0
-loadImage quay.io/jetstack/cert-manager-webhook:v1.11.0
-loadImage quay.io/prometheus/prometheus:v2.41.0
-loadImage registry.k8s.io/ingress-nginx/controller:v1.5.1@sha256:4ba73c697770664c1e00e9f968de14e08f606ff961c76e5d7033a4a9c593c629
+loadImage quay.io/brancz/kube-rbac-proxy:v0.15.0
+loadImage quay.io/jetstack/cert-manager-cainjector:v1.13.3
+loadImage quay.io/jetstack/cert-manager-controller:v1.13.3
+loadImage quay.io/jetstack/cert-manager-webhook:v1.13.3
+loadImage quay.io/prometheus-operator/prometheus-config-reloader:v0.70.0
+loadImage quay.io/prometheus/prometheus:v2.48.1
+loadImage registry.k8s.io/ingress-nginx/controller:v1.9.5@sha256:b3aba22b1da80e7acfc52b115cae1d4c687172cbf2b742d5b502419c25ff340e
