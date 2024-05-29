89ab68a75fcd52f06809c3580209ce3471b3a4e3
Tiago Angelo <kurtis.angelo@gmail.com>
2024-05-29 10:44:59 -0300

    Update cluster_host calculation and add kind version variable

--- kind-keycloak/locals.tf
+++ kind-keycloak/locals.tf
@@ -1,8 +1,8 @@
 locals {
-  ingress_ip       = "127.0.0.1"
-  cluster_host     = local.ingress_ip == "127.0.0.1" ? "lvh.me" : "${join("", formatlist("%02x", split(".", local.ingress_ip)))}.nip.io"
+  cluster_host = var.load_balancer_address == "127.0.0.1" ? "lvh.me" : "${join("", formatlist("%02x", split(".", var.load_balancer_address)))}.nip.io"
+  kind = { version = "v1.29.4" }
   default_timeouts = "5m"
-  fluxcd           = {
+  fluxcd = {
     version          = "v0.28.5"
     default_interval = "60m"
     default_timeout  = "5m"
