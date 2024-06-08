455e868c52149d8d6cb453f06918d39e0dc0a987
Tiago Angelo <kurtis.angelo@gmail.com>
2024-06-08 15:42:31 -0300

    Update provider versions in providers.tf

--- kind-keycloak/providers.tf
+++ kind-keycloak/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    kind       = { source = "tehcyx/kind", version = ">= 0.2.1, < 0.3.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.25.1, < 4.0.0" }
-    helm       = { source = "hashicorp/helm", version = ">= 2.12.1, < 3.0.0" }
+    kind       = { source = "tehcyx/kind", version = ">= 0.5.1, < 1.0.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.30.0, < 4.0.0" }
+    helm       = { source = "hashicorp/helm", version = ">= 2.13.2, < 3.0.0" }
     kubectl    = { source = "alekc/kubectl", version = ">= 2.0.4, < 3.0.0" }
     null       = { source = "hashicorp/null", version = ">= 3.2.2, < 4.0.0" }
   }
