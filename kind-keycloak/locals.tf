12333bfeca06a59b5b2b228e9bde52413a753015
Tiago Angelo <kurtis.angelo@gmail.com>
2024-07-18 20:16:56 -0300

    Update Kind version to v1.30.2 and remove unnecessary values

--- kind-keycloak/locals.tf
+++ kind-keycloak/locals.tf
@@ -1,12 +1,8 @@
 locals {
   cluster_host = var.load_balancer_address == "127.0.0.1" ? "lvh.me" : "${join("", formatlist("%02x", split(".", var.load_balancer_address)))}.nip.io"
-  kind = { version = "v1.29.4" }
-  default_timeouts = "5m"
+  kind = { version = "v1.30.2" }
   fluxcd = {
-    version          = "v0.28.5"
     default_interval = "60m"
-    default_timeout  = "5m"
-    namespace        = "fluxcd"
   }
   keycloak = {
     host      = "keycloak.${local.cluster_host}"
