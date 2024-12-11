b799cb216e97963e6db71e07dd4727b2c9621ab7
Tiago Angelo <kurtis.angelo@gmail.com>
2024-12-11 10:39:43 -0300

    Add providers configuration for kind-crossplane setup

---
+++ kind-crossplane/providers.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    kind       = { source = "tehcyx/kind", version = ">= 0.2.1, < 0.3.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.25.1, < 4.0.0" }
+    helm       = { source = "hashicorp/helm", version = ">= 2.12.1, < 3.0.0" }
+    kubectl    = { source = "alekc/kubectl", version = ">= 2.0.4, < 3.0.0" }
+    null       = { source = "hashicorp/null", version = ">= 3.2.2, < 4.0.0" }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "kind" {}
+
+provider "kubernetes" {
+  host = kind_cluster.crossplane.endpoint
+
+  client_certificate     = kind_cluster.crossplane.client_certificate
+  client_key             = kind_cluster.crossplane.client_key
+  cluster_ca_certificate = kind_cluster.crossplane.cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host = kind_cluster.crossplane.endpoint
+
+    client_certificate     = kind_cluster.crossplane.client_certificate
+    client_key             = kind_cluster.crossplane.client_key
+    cluster_ca_certificate = kind_cluster.crossplane.cluster_ca_certificate
+  }
+}
+
+
+provider "kubectl" {
+  host = kind_cluster.crossplane.endpoint
+
+  client_certificate     = kind_cluster.crossplane.client_certificate
+  client_key             = kind_cluster.crossplane.client_key
+  cluster_ca_certificate = kind_cluster.crossplane.cluster_ca_certificate
+  load_config_file       = false
+}
