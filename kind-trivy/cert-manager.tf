03a96076b22c3df585449aafa90d5789e8f20ac7
Tiago Angelo <kurtis.angelo@gmail.com>
2025-02-17 14:13:28 -0300

    Add Terraform configurations for kind-trivy setup

---
+++ kind-trivy/cert-manager.tf
@@ -0,0 +1,124 @@
+locals {
+  cert_manager_crds = [
+    "customresourcedefinition.apiextensions.k8s.io/certificaterequests.cert-manager.io",
+    "customresourcedefinition.apiextensions.k8s.io/certificates.cert-manager.io",
+    "customresourcedefinition.apiextensions.k8s.io/challenges.acme.cert-manager.io",
+    "customresourcedefinition.apiextensions.k8s.io/clusterissuers.cert-manager.io",
+    "customresourcedefinition.apiextensions.k8s.io/issuers.cert-manager.io",
+    "customresourcedefinition.apiextensions.k8s.io/orders.acme.cert-manager.io",
+  ]
+}
+
+resource "kubectl_manifest" "helm_repository_jetstack" {
+  yaml_body = <<-YAML
+    apiVersion: source.toolkit.fluxcd.io/v1beta2
+    kind: HelmRepository
+    metadata:
+      name: jetstack
+      namespace: ${kubernetes_namespace.flux.metadata[0].name}
+    spec:
+      interval: 60s
+      url: https://charts.jetstack.io
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_cert_manager_crd]
+}
+
+resource "kubectl_manifest" "helm_release_cert_manager" {
+  yaml_body = <<-YAML
+    apiVersion: helm.toolkit.fluxcd.io/v2beta1
+    kind: HelmRelease
+    metadata:
+      name: cert-manager
+      namespace: ${kubernetes_namespace.cert_manager.metadata[0].name}
+      annotations:
+        "checksum/config": ${sha256(kubernetes_config_map_v1.cert_manager_helm_values.data["values.yaml"])}
+    spec:
+      chart:
+        spec:
+          chart: cert-manager
+          reconcileStrategy: ChartVersion
+          sourceRef:
+            kind: HelmRepository
+            name: jetstack
+            namespace: ${kubernetes_namespace.flux.metadata[0].name}
+      valuesFrom:
+        - kind: ConfigMap
+          name: ${kubernetes_config_map_v1.cert_manager_helm_values.metadata[0].name}
+      interval: 60s
+  YAML
+
+  depends_on = [kubernetes_job_v1.wait_cert_manager_crd]
+}
+
+resource "kubernetes_config_map_v1" "cert_manager_helm_values" {
+  metadata {
+    name      = "cert-manager-helm-values"
+    namespace = kubernetes_namespace.cert_manager.metadata[0].name
+  }
+  data = {
+    "values.yaml" = yamlencode({
+      installCRDs = true
+      prometheus = { enabled = false }
+    })
+  }
+}
+
+resource "kubernetes_job_v1" "wait_cert_manager_crd" {
+  metadata {
+    name      = "wait-cert-manager-crd"
+    namespace = kubernetes_namespace.flux.metadata[0].name
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        service_account_name = kubernetes_service_account_v1.wait_cert_manager_crd.metadata[0].name
+        container {
+          name  = "kubectl"
+          image = "docker.io/bitnami/kubectl:${data.kubectl_server_version.current.major}.${data.kubectl_server_version.current.minor}"
+          args  = flatten(["wait", "--for=condition=Established", local.flux_crds, "--timeout", "10m"])
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+  wait_for_completion = true
+
+  timeouts {
+    create = "10m"
+    update = "10m"
+  }
+
+  depends_on = [
+    helm_release.flux,
+    kubernetes_cluster_role_binding_v1.wait_cert_manager_crd,
+  ]
+}
+
+resource "kubernetes_service_account_v1" "wait_cert_manager_crd" {
+  metadata {
+    name      = "wait-cert-manager-crd"
+    namespace = kubernetes_namespace.flux.metadata[0].name
+  }
+}
+
+resource "kubernetes_cluster_role_binding_v1" "wait_cert_manager_crd" {
+  metadata {
+    name = "wait-cert-manager-crd"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role_v1.crd_reader.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account_v1.wait_cert_manager_crd.metadata[0].name
+    namespace = kubernetes_service_account_v1.wait_cert_manager_crd.metadata[0].namespace
+  }
+}
+
+resource "kubernetes_namespace" "cert_manager" {
+  metadata { name = "cert-manager" }
+}
---
+++ kind-trivy/providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    kind       = { source = "tehcyx/kind", version = ">= 0.7.0, < 1.0.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.35.1, < 4.0.0" }
+    helm       = { source = "hashicorp/helm", version = ">= 2.17.0, < 3.0.0" }
+    kubectl    = { source = "alekc/kubectl", version = ">= 2.1.3, < 3.0.0" }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "kind" {}
+
+provider "kubernetes" {
+  host = kind_cluster.trivy.endpoint
+
+  client_certificate     = kind_cluster.trivy.client_certificate
+  client_key             = kind_cluster.trivy.client_key
+  cluster_ca_certificate = kind_cluster.trivy.cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host = kind_cluster.trivy.endpoint
+
+    client_certificate     = kind_cluster.trivy.client_certificate
+    client_key             = kind_cluster.trivy.client_key
+    cluster_ca_certificate = kind_cluster.trivy.cluster_ca_certificate
+  }
+}
+
+
+provider "kubectl" {
+  host = kind_cluster.trivy.endpoint
+
+  client_certificate     = kind_cluster.trivy.client_certificate
+  client_key             = kind_cluster.trivy.client_key
+  cluster_ca_certificate = kind_cluster.trivy.cluster_ca_certificate
+  load_config_file       = false
+}
