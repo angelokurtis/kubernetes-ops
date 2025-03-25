55ab04cdb328880d95182dd0ecb1d193d942fbd0
Tiago Angelo <kurtis.angelo@gmail.com>
2025-03-25 11:54:13 -0300

    Update Kubernetes and Helm provider versions

--- kind-prometheus/providers.tf
+++ kind-prometheus/providers.tf
@@ -1,26 +1,21 @@
 terraform {
   required_providers {
-    docker     = { source = "kreuzwerker/docker", version = ">= 2.17.0, < 2.18.0" }
-    kind       = { source = "tehcyx/kind", version = ">= 0.0.13, < 0.1.0" }
-    flux       = { source = "fluxcd/flux", version = ">= 0.15.3, < 0.16.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.12.1, < 2.13.0" }
-    kubectl    = { source = "gavinbunney/kubectl", version = ">= 1.14.0, < 1.15.0" }
+    kubectl = { source = "alekc/kubectl", version = ">= 2.1.3, < 3.0.0" }
+    helm = { source = "hashicorp/helm", version = ">= 2.17.0, < 3.0.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.36.0, < 4.0.0" }
+    kind = { source = "tehcyx/kind", version = ">= 0.8.0, < 1.0.0" }
   }
   required_version = ">= 1.0"
 }
 
-provider "docker" {}
+provider "helm" {
+  kubernetes {
+    host = kind_cluster.metrics.endpoint
 
-provider "kind" {}
-
-provider "flux" {}
-
-provider "kubernetes" {
-  host = kind_cluster.metrics.endpoint
-
-  client_certificate     = kind_cluster.metrics.client_certificate
-  client_key             = kind_cluster.metrics.client_key
-  cluster_ca_certificate = kind_cluster.metrics.cluster_ca_certificate
+    client_certificate     = kind_cluster.metrics.client_certificate
+    client_key             = kind_cluster.metrics.client_key
+    cluster_ca_certificate = kind_cluster.metrics.cluster_ca_certificate
+  }
 }
 
 provider "kubectl" {
@@ -31,3 +26,13 @@ provider "kubectl" {
   cluster_ca_certificate = kind_cluster.metrics.cluster_ca_certificate
   load_config_file       = false
 }
+
+provider "kubernetes" {
+  host = kind_cluster.metrics.endpoint
+
+  client_certificate     = kind_cluster.metrics.client_certificate
+  client_key             = kind_cluster.metrics.client_key
+  cluster_ca_certificate = kind_cluster.metrics.cluster_ca_certificate
+}
+
+provider "kind" {}
