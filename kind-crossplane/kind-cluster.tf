f1a5e73fd3a63807303693d64b3325a640ca4f75
Tiago Angelo <kurtis.angelo@gmail.com>
2025-06-16 06:12:04 -0300

    Update Kind version to v1.33.1 in cluster configuration

--- kind-crossplane/kind-cluster.tf
+++ kind-crossplane/kind-cluster.tf
@@ -1,6 +1,6 @@
 locals {
   cluster_host = var.load_balancer_address == "127.0.0.1" ? "lvh.me" : "${join("", formatlist("%02x", split(".", var.load_balancer_address)))}.nip.io"
-  kind         = { version = "v1.31.4" }
+  kind         = { version = "v1.33.1" }
 }
 
 resource "kind_cluster" "crossplane" {
