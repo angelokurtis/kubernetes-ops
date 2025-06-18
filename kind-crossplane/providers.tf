1e007760a88e92ca10149cc45ea235efa9c82881
Tiago Angelo <kurtis.angelo@gmail.com>
2025-06-18 18:35:19 -0300

    Add kustomization provider to providers.tf

--- kind-crossplane/providers.tf
+++ kind-crossplane/providers.tf
@@ -4,6 +4,7 @@ terraform {
     kind = { source = "tehcyx/kind", version = "~> 0.9" }
     kubectl = { source = "alekc/kubectl", version = "~> 2.1" }
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.37" }
+    kustomization = { source = "kbst/kustomization", version = "~> 0.9" }
     null = { source = "hashicorp/null", version = "~> 3.2" }
   }
   required_version = ">= 1.9"
@@ -36,6 +37,10 @@ provider "kubernetes" {
   client_certificate     = kind_cluster.crossplane.client_certificate
   client_key             = kind_cluster.crossplane.client_key
   cluster_ca_certificate = kind_cluster.crossplane.cluster_ca_certificate
+}
+
+provider "kustomization" {
+  kubeconfig_raw = kind_cluster.crossplane.kubeconfig
 }
 
 provider "null" {
