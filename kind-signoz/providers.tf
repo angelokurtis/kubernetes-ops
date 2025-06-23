98de0dd315de28b7f0fd4533b5213fb4df11e9a8
Tiago Angelo <kurtis.angelo@gmail.com>
2025-06-23 11:39:57 -0300

    Add Terraform scripts for kind Signoz setup

---
+++ kind-signoz/providers.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    helm = { source = "hashicorp/helm", version = "~> 2.17" }
+    kind = { source = "tehcyx/kind", version = "~> 0.9" }
+    kubectl = { source = "alekc/kubectl", version = "~> 2.1" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.37" }
+    kustomization = { source = "kbst/kustomization", version = "~> 0.9" }
+    null = { source = "hashicorp/null", version = "~> 3.2" }
+  }
+  required_version = ">= 1.9"
+}
+
+provider "helm" {
+  kubernetes {
+    host = kind_cluster.signoz.endpoint
+
+    client_certificate     = kind_cluster.signoz.client_certificate
+    client_key             = kind_cluster.signoz.client_key
+    cluster_ca_certificate = kind_cluster.signoz.cluster_ca_certificate
+  }
+}
+
+provider "kind" {}
+
+provider "kubectl" {
+  host = kind_cluster.signoz.endpoint
+
+  client_certificate     = kind_cluster.signoz.client_certificate
+  client_key             = kind_cluster.signoz.client_key
+  cluster_ca_certificate = kind_cluster.signoz.cluster_ca_certificate
+  load_config_file       = false
+}
+
+provider "kubernetes" {
+  host = kind_cluster.signoz.endpoint
+
+  client_certificate     = kind_cluster.signoz.client_certificate
+  client_key             = kind_cluster.signoz.client_key
+  cluster_ca_certificate = kind_cluster.signoz.cluster_ca_certificate
+}
+
+provider "kustomization" {
+  kubeconfig_raw = kind_cluster.signoz.kubeconfig
+}
+
+provider "null" {
+}
