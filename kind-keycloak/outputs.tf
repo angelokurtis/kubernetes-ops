84cb9e898fb6da030f5e4babe2fbdbe905c6d49e
Tiago Angelo <kurtis.angelo@gmail.com>
2025-07-06 16:56:05 -0300

    Add Keycloak access info output to Makefile

---
+++ kind-keycloak/outputs.tf
@@ -0,0 +1,4 @@
+output "keycloak_access_info" {
+  value     = "Access Keycloak at http://${yamldecode(kubernetes_config_map_v1.keycloak_helm_values.data["values.yaml"]).ingress.hostname}/ using username ${yamldecode(kubernetes_config_map_v1.keycloak_helm_values.data["values.yaml"]).auth.adminUser} and password ${kubernetes_secret_v1.keycloak_passwords.data["admin-password"]}."
+  sensitive = true
+}
