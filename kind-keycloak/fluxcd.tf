b811b7faabedbe20eda0e1f4511460ba1facb807
Tiago Angelo <kurtis.angelo@gmail.com>
2025-07-06 10:24:29 -0300

    Update flux version and add new flux CRDs

--- kind-keycloak/fluxcd.tf
+++ kind-keycloak/fluxcd.tf
@@ -1,30 +1,28 @@
 locals {
   flux_crds = [
+    "customresourcedefinition.apiextensions.k8s.io/alerts.notification.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/buckets.source.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/gitrepositories.source.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/helmcharts.source.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/helmreleases.helm.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/helmrepositories.source.toolkit.fluxcd.io",
+    "customresourcedefinition.apiextensions.k8s.io/imagepolicies.image.toolkit.fluxcd.io",
+    "customresourcedefinition.apiextensions.k8s.io/imagerepositories.image.toolkit.fluxcd.io",
+    "customresourcedefinition.apiextensions.k8s.io/imageupdateautomations.image.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/kustomizations.kustomize.toolkit.fluxcd.io",
     "customresourcedefinition.apiextensions.k8s.io/ocirepositories.source.toolkit.fluxcd.io",
+    "customresourcedefinition.apiextensions.k8s.io/providers.notification.toolkit.fluxcd.io",
+    "customresourcedefinition.apiextensions.k8s.io/receivers.notification.toolkit.fluxcd.io",
   ]
 }
 
 resource "helm_release" "flux" {
   repository = "https://fluxcd-community.github.io/helm-charts"
   chart      = "flux2"
-  version    = "2.13.0"
+  version    = "2.16.1"
 
   name      = "flux"
   namespace = kubernetes_namespace.flux.metadata[0].name
-
-  values = [
-    yamlencode({
-      imageAutomationController = { create = false }
-      imageReflectionController = { create = false }
-      notificationController = { create = false }
-    })
-  ]
 }
 
 resource "kubernetes_job_v1" "wait_flux_crd" {
@@ -40,7 +38,7 @@ resource "kubernetes_job_v1" "wait_flux_crd" {
         container {
           name  = "kubectl"
           image = "docker.io/bitnami/kubectl:${data.kubectl_server_version.current.major}.${data.kubectl_server_version.current.minor}"
-          args = flatten(["wait", "--for=condition=Established", local.flux_crds, "--timeout", "10m"])
+          args  = flatten(["wait", "--for=condition=Established", local.flux_crds, "--timeout", "10m"])
         }
         restart_policy = "Never"
       }
