e76ebd29d5085d750f02621f7d210debceaaed7a
Tiago Angelo <kurtis.angelo@gmail.com>
2025-07-30 15:31:18 -0300

    Update flux helm chart version to 2.16.3

--- kind-testkube/flux.tf
+++ kind-testkube/flux.tf
@@ -19,7 +19,7 @@ locals {
 resource "helm_release" "flux" {
   repository = "https://fluxcd-community.github.io/helm-charts"
   chart      = "flux2"
-  version    = "2.9.2"
+  version    = "2.16.3"
 
   name      = "flux"
   namespace = kubernetes_namespace.flux.metadata[0].name
