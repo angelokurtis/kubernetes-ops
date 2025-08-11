4c50ea40eca9ca640e86ff1f159c2b8506b68616
Tiago Angelo <kurtis.angelo@gmail.com>
2025-08-11 16:05:52 -0300

    Update Crossplane Helm chart version to ^1.20.0

--- kind-crossplane/crossplane.tf
+++ kind-crossplane/crossplane.tf
@@ -27,7 +27,7 @@ resource "kubectl_manifest" "helm_release_crossplane" {
       chart:
         spec:
           chart: crossplane
-          version: 1.20.1
+          version: ^1.20.0
           reconcileStrategy: ChartVersion
           sourceRef:
             kind: HelmRepository
