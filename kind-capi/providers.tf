545d9d6fd75516627588812c8b684fb6a76d5bdb
Tiago Angelo <kurtis.angelo@gmail.com>
2025-08-28 05:25:57 -0300

    Add new providers in terraform config

--- kind-capi/providers.tf
+++ kind-capi/providers.tf
@@ -1,21 +1,13 @@
 terraform {
   required_providers {
-    kind       = { source = "tehcyx/kind", version = "~> 0.9" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.38" }
-    helm       = { source = "hashicorp/helm", version = "~> 2.17" }
-    kubectl    = { source = "alekc/kubectl", version = "~> 2.1" }
+    helm          = { source = "hashicorp/helm", version = "~> 2.17" }
+    kind          = { source = "tehcyx/kind", version = "~> 0.9" }
+    kubectl       = { source = "alekc/kubectl", version = "~> 2.1" }
+    kubernetes    = { source = "hashicorp/kubernetes", version = "~> 2.38" }
+    kustomization = { source = "kbst/kustomization", version = "~> 0.9" }
+    null          = { source = "hashicorp/null", version = "~> 3.2" }
   }
   required_version = ">= 1.9"
-}
-
-provider "kind" {}
-
-provider "kubernetes" {
-  host = kind_cluster.capi.endpoint
-
-  client_certificate     = kind_cluster.capi.client_certificate
-  client_key             = kind_cluster.capi.client_key
-  cluster_ca_certificate = kind_cluster.capi.cluster_ca_certificate
 }
 
 provider "helm" {
@@ -28,6 +20,7 @@ provider "helm" {
   }
 }
 
+provider "kind" {}
 
 provider "kubectl" {
   host = kind_cluster.capi.endpoint
@@ -36,4 +29,19 @@ provider "kubectl" {
   client_key             = kind_cluster.capi.client_key
   cluster_ca_certificate = kind_cluster.capi.cluster_ca_certificate
   load_config_file       = false
+}
+
+provider "kubernetes" {
+  host = kind_cluster.capi.endpoint
+
+  client_certificate     = kind_cluster.capi.client_certificate
+  client_key             = kind_cluster.capi.client_key
+  cluster_ca_certificate = kind_cluster.capi.cluster_ca_certificate
+}
+
+provider "kustomization" {
+  kubeconfig_raw = kind_cluster.capi.kubeconfig
+}
+
+provider "null" {
 }
