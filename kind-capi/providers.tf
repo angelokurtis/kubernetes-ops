20960b2bae3d5a61db7778050fe368bbf2da0410
Tiago Angelo <kurtis.angelo@gmail.com>
2025-08-29 18:08:04 -0300

    Remove kustomization provider from providers.tf

--- kind-capi/providers.tf
+++ kind-capi/providers.tf
@@ -1,11 +1,10 @@
 terraform {
   required_providers {
-    helm          = { source = "hashicorp/helm", version = "~> 2.17" }
-    kind          = { source = "tehcyx/kind", version = "~> 0.9" }
-    kubectl       = { source = "alekc/kubectl", version = "~> 2.1" }
-    kubernetes    = { source = "hashicorp/kubernetes", version = "~> 2.38" }
-    kustomization = { source = "kbst/kustomization", version = "~> 0.9" }
-    null          = { source = "hashicorp/null", version = "~> 3.2" }
+    helm       = { source = "hashicorp/helm", version = "~> 2.17" }
+    kind       = { source = "tehcyx/kind", version = "~> 0.9" }
+    kubectl    = { source = "alekc/kubectl", version = "~> 2.1" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.38" }
+    null       = { source = "hashicorp/null", version = "~> 3.2" }
   }
   required_version = ">= 1.9"
 }
@@ -37,10 +36,6 @@ provider "kubernetes" {
   client_certificate     = kind_cluster.capi.client_certificate
   client_key             = kind_cluster.capi.client_key
   cluster_ca_certificate = kind_cluster.capi.cluster_ca_certificate
-}
-
-provider "kustomization" {
-  kubeconfig_raw = kind_cluster.capi.kubeconfig
 }
 
 provider "null" {
