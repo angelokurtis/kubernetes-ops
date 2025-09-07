a0044234b5dd7d45ae94f2dd726e3c0b56aff064
Tiago Angelo <kurtis.angelo@gmail.com>
2025-09-07 18:33:53 -0300

    Update Proxmox image file and datastore setup

--- proxmox-cluster/image.tf
+++ proxmox-cluster/image.tf
@@ -5,6 +5,11 @@ locals {
     arch        = "amd64"
     version     = "v1.11.0"
   }
+  proxmox_node_name = "pve"
+  proxmox_iso_datastore = one([
+    for ds in data.proxmox_virtual_environment_datastores._.datastores :
+    ds if contains(ds["content_types"], "iso")
+  ])
 }
 
 data "http" "schematic_id" {
@@ -13,15 +18,16 @@ data "http" "schematic_id" {
   request_body = file("${path.module}/image/schematic.yaml")
 }
 
-resource "proxmox_virtual_environment_download_file" "talos_image" {
-  node_name               = "pve"
-  content_type            = "iso"
-  datastore_id            = "local"
-  decompression_algorithm = "gz"
-  overwrite               = false
+data "proxmox_virtual_environment_datastores" "_" {
+  node_name = local.proxmox_node_name
+}
 
-  url       = "${local.talos.factory_url}/image/${jsondecode(data.http.schematic_id.response_body)["id"]}/${local.talos.version}/${local.talos.platform}-${local.talos.arch}.raw.gz"
-  file_name = "talos-${jsondecode(data.http.schematic_id.response_body)["id"]}-${local.talos.version}-${local.talos.platform}-${local.talos.arch}.img"
+resource "proxmox_virtual_environment_download_file" "talos_image" {
+  node_name    = local.proxmox_node_name
+  url          = "${local.talos.factory_url}/image/${jsondecode(data.http.schematic_id.response_body)["id"]}/${local.talos.version}/${local.talos.platform}-${local.talos.arch}.iso"
+  file_name    = "talos-${jsondecode(data.http.schematic_id.response_body)["id"]}-${local.talos.version}-${local.talos.platform}-${local.talos.arch}.iso"
+  content_type = "iso"
+  datastore_id = local.proxmox_iso_datastore["id"]
 }
 
 resource "talos_machine_secrets" "_" {
