5c21e2668ba0715be357bed6405d6fa22a8cc57e
Tiago Angelo <kurtis.angelo@gmail.com>
2025-09-07 10:52:33 -0300

    Add cluster and nodes variables

---
+++ proxmox-cluster/variables.tf
@@ -0,0 +1,26 @@
+variable "cluster" {
+  description = "Cluster configuration"
+  type = object({
+    name            = string
+    endpoint        = string
+    gateway         = string
+    talos_version   = string
+    proxmox_cluster = string
+  })
+}
+
+variable "nodes" {
+  description = "Configuration for cluster nodes"
+  type = map(object({
+    host_node     = string
+    machine_type  = string
+    datastore_id  = optional(string, "local-zfs")
+    ip            = string
+    mac_address   = string
+    vm_id         = number
+    cpu           = number
+    ram_dedicated = number
+    update        = optional(bool, false)
+    igpu          = optional(bool, false)
+  }))
+}
