cb2f61b5c8effdc7ff8916d02fd233855d99194b
Tiago Angelo <kurtis.angelo@gmail.com>
2025-09-07 21:29:52 -0300

    Refactor proxmox-cluster variables for simplicity

--- proxmox-cluster/variables.tf
+++ proxmox-cluster/variables.tf
@@ -14,29 +14,14 @@ variable "proxmox_password" {
   sensitive   = true
 }
 
-variable "cluster" {
-  description = "Cluster configuration"
-  type = object({
-    name            = string
-    endpoint        = string
-    gateway         = string
-    talos_version   = string
-    proxmox_cluster = string
-  })
+variable "control_plane_count" {
+  description = "Number of Kubernetes control plane nodes"
+  type        = number
+  default     = 1
 }
 
-variable "nodes" {
-  description = "Configuration for cluster nodes"
-  type = map(object({
-    host_node     = string
-    machine_type  = string
-    datastore_id  = optional(string, "local-zfs")
-    ip            = string
-    mac_address   = string
-    vm_id         = number
-    cpu           = number
-    ram_dedicated = number
-    update        = optional(bool, false)
-    igpu          = optional(bool, false)
-  }))
+variable "worker_count" {
+  description = "Number of Kubernetes worker nodes"
+  type        = number
+  default     = 2
 }
