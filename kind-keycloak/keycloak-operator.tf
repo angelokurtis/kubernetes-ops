6850f51074e73c3fa8b34f49dc900b83fdc72448
Tiago Angelo <kurtis.angelo@gmail.com>
2025-09-21 11:33:13 -0300

    Update namespace in keycloak-operator.tf file

--- kind-keycloak/keycloak-operator.tf
+++ kind-keycloak/keycloak-operator.tf
@@ -9,13 +9,13 @@ resource "kubectl_manifest" "git_repository_keycloak_operator" {
 
 resource "kubectl_manifest" "kustomization_keycloak_operator" {
   yaml_body = templatefile("${path.module}/manifests/kustomizations.kustomize.toolkit.fluxcd.io/keycloak-operator.yaml", {
-    namespace            = kubernetes_namespace.keycloak.metadata[0].name
+    namespace            = kubernetes_namespace.keycloak_operator.metadata[0].name
     source_ref_namespace = kubernetes_namespace.flux.metadata[0].name
   })
 
   depends_on = [kubernetes_job_v1.wait_flux_crd]
 }
 
-resource "kubernetes_namespace" "keycloak" {
-  metadata { name = "keycloak" }
+resource "kubernetes_namespace" "keycloak_operator" {
+  metadata { name = "keycloak-operator" }
 }
