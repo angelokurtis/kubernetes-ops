6b18d3a3353d3ed8a5d48b3671f63cb4a615cbd5
Tiago Angelo <kurtis.angelo@gmail.com>
2025-09-21 18:08:34 -0300

    Add kubectl manifest for Keycloak and update DB secrets

--- kind-keycloak/keycloak.tf
+++ kind-keycloak/keycloak.tf
@@ -1,3 +1,19 @@
+resource "kubectl_manifest" "keycloak" {
+  yaml_body = templatefile("${path.module}/manifests/keycloaks.k8s.keycloak.org/keycloak.yaml", {
+    namespace             = kubernetes_namespace.keycloak.metadata[0].name
+    host                  = "keycloak.${local.cluster_host}"
+    keycloak_admin_secret = kubernetes_secret_v1.keycloak_admin.metadata[0].name
+    postgresql_secret    = kubernetes_secret_v1.keycloak_db.metadata[0].name
+    postgresql_service   = kubernetes_service_v1.keycloak_postgresql.metadata[0].name
+    postgresql_namespace = kubernetes_namespace.postgresql.metadata[0].name
+  })
+
+  depends_on = [
+    kubectl_manifest.kustomization_keycloak_operator,
+    kubernetes_stateful_set_v1.postgresql_db,
+  ]
+}
+
 resource "random_password" "keycloak_admin" {
   length = 16
 }
