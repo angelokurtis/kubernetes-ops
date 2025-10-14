d28f361059a82b2fd64ad655f2a8ce540b2be5f6
Tiago Angelo <kurtis.angelo@gmail.com>
2025-10-14 17:47:56 -0300

    Update semver for keycloak operator to ^26.4.0

--- kind-keycloak/keycloak-operator.tf
+++ kind-keycloak/keycloak-operator.tf
@@ -1,7 +1,7 @@
 resource "kubectl_manifest" "git_repository_keycloak_operator" {
   yaml_body = templatefile("${path.module}/manifests/gitrepositories.source.toolkit.fluxcd.io/keycloak-k8s-resources.yaml", {
     namespace = kubernetes_namespace.flux.metadata[0].name
-    semver    = "^26.3.4"
+    semver    = "^26.4.0"
   })
 
   depends_on = [kubernetes_job_v1.wait_flux_crd]
