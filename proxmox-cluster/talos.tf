e028760b252f2bebe381700466ceb62a904cc1a1
Tiago Angelo <kurtis.angelo@gmail.com>
2025-10-26 15:41:47 -0300

    Refactor talos.tf to use local variables for IPs

--- proxmox-cluster/talos.tf
+++ proxmox-cluster/talos.tf
@@ -2,16 +2,7 @@ locals {
   cluster_name = "homelab-talos-dev"
   cluster_endpoint = format(
     "https://%s:6443",
-    proxmox_virtual_environment_vm.control_plane[0].ipv4_addresses[
-      index(
-        proxmox_virtual_environment_vm.control_plane[0].mac_addresses,
-        one([
-          for nd in proxmox_virtual_environment_vm.control_plane[0].network_device :
-          nd.mac_address
-          if nd.bridge == "vmbr0"
-        ])
-      )
-    ][0]
+    values(local.control_planes)[0].ip
   )
 }
 
@@ -20,18 +11,6 @@ resource "talos_machine_secrets" "_" {}
 data "talos_client_configuration" "_" {
   cluster_name         = local.cluster_name
   client_configuration = talos_machine_secrets._.client_configuration
-  nodes = concat(
-    [
-      for vm in proxmox_virtual_environment_vm.control_plane :
-      [for ip in flatten(vm.ipv4_addresses) : ip if ip != "127.0.0.1"][0]
-    ],
-    [
-      for vm in proxmox_virtual_environment_vm.worker :
-      [for ip in flatten(vm.ipv4_addresses) : ip if ip != "127.0.0.1"][0]
-    ]
-  )
-  endpoints = [
-    for vm in proxmox_virtual_environment_vm.control_plane :
-    [for ip in flatten(vm.ipv4_addresses) : ip if ip != "127.0.0.1"][0]
-  ]
+  nodes                = concat(values(local.control_planes)[*].ip, values(local.workers)[*].ip)
+  endpoints            = values(local.control_planes)[*].ip
 }
