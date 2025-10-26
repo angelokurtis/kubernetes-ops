41de598d2dc1eea169fd32d7e48997e2a6e8c289
Tiago Angelo <kurtis.angelo@gmail.com>
2025-10-26 16:10:56 -0300

    Add null_resource to write talos config locally

--- proxmox-cluster/providers.tf
+++ proxmox-cluster/providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     http       = { source = "hashicorp/http", version = "~> 3.5" }
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.38" }
+    null       = { source = "hashicorp/null", version = "~> 3.2" }
     proxmox    = { source = "bpg/proxmox", version = "~> 0.83" }
-    restapi    = { source = "Mastercard/restapi", version = "~> 2.0" }
     talos      = { source = "siderolabs/talos", version = "~> 0.9" }
   }
   required_version = ">= 1.9"
--- proxmox-cluster/talos.tf
+++ proxmox-cluster/talos.tf
@@ -14,3 +14,19 @@ data "talos_client_configuration" "_" {
   nodes                = concat(values(local.control_planes)[*].ip, values(local.workers)[*].ip)
   endpoints            = values(local.control_planes)[*].ip
 }
+
+resource "null_resource" "write_talos_config" {
+  triggers = {
+    talos_config = data.talos_client_configuration._.talos_config
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      mkdir -p "$HOME/.talos"
+      cat > "$HOME/.talos/config" <<EOF
+${data.talos_client_configuration._.talos_config}
+EOF
+      chmod 600 "$HOME/.talos/config"
+    EOT
+  }
+}
