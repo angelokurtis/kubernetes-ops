73e906a930092841176fb76e09a0c7f659c93c60
Tiago Angelo <kurtis.angelo@gmail.com>
2025-11-05 09:21:40 -0300

    Update flux2 helm chart version to 2.17.1

--- kind-keycloak/fluxcd.tf
+++ kind-keycloak/fluxcd.tf
@@ -13,7 +13,7 @@ locals {
 resource "helm_release" "flux" {
   repository = "https://fluxcd-community.github.io/helm-charts"
   chart      = "flux2"
-  version    = "2.16.4"
+  version    = "2.17.1"
 
   name      = "flux"
   namespace = kubernetes_namespace.flux.metadata[0].name
