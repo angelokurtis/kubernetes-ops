799e3f3327850f6714f87798369ccb35f31967ff
Tiago Angelo <kurtis.angelo@gmail.com>
2026-04-01 17:31:13 -0300

    Support multi-namespace OpenTofu provider deployment

--- kind-crossplane/provider-opentofu.tf
+++ kind-crossplane/provider-opentofu.tf
@@ -1,58 +1,84 @@
+locals {
+  namespaces = ["vms", "vpc"]
+}
+
+resource "kubernetes_namespace_v1" "namespaces" {
+  for_each = toset(local.namespaces)
+
+  metadata { name = each.value }
+}
+
 data "kustomization_overlay" "provider_opentofu" {
+  for_each = toset(local.namespaces)
+
   resources = ["kustomize/crossplane-provider-opentofu/"]
 
   patches {
     patch = jsonencode([
       {
         op    = "replace"
-        path  = "/spec/package"
-        value = "xpkg.upbound.io/upbound/provider-opentofu:${local.provider_opentofu_latest_release}"
+        path  = "/metadata/name"
+        value = "crossplane-provider-opentofu-${each.value}"
       },
+      {
+        op    = "replace"
+        path  = "/spec/package"
+        value = "docker.io/kurtis/crossplane-opentofu-provider:060c989"
+      },
+      {
+        op    = "replace"
+        path  = "/spec/runtimeConfigRef/name"
+        value = "opentofu-config-${each.value}"
+      }
     ])
     target {
       group = "pkg.crossplane.io"
       kind  = "Provider"
+      name  = "crossplane-provider-opentofu"
+    }
+  }
+
+  patches {
+    patch = jsonencode([
+      {
+        op    = "replace"
+        path  = "/metadata/name"
+        value = "opentofu-config-${each.value}"
+      },
+      {
+        op   = "add"
+        path = "/spec/deploymentTemplate/spec/template/spec/containers"
+        value = [
+          {
+            name = "package-runtime",
+            env = [
+              { name = "WATCH_NAMESPACE", value = each.value },
+              { name = "USER", value = "crossplane" }
+            ]
+          }
+        ]
+      }
+    ])
+    target {
+      group = "pkg.crossplane.io"
+      kind  = "DeploymentRuntimeConfig"
+      name  = "opentofu-config"
     }
   }
 }
 
-resource "kustomization_resource" "provider_opentofu_p0" {
-  for_each = data.kustomization_overlay.provider_opentofu.ids_prio[0]
+resource "kustomization_resource" "provider_opentofu" {
+  for_each = merge([
+    for ns, overlay in data.kustomization_overlay.provider_opentofu :
+    {
+      for id in overlay.ids :
+      "${ns}-${id}" => {
+        manifest = overlay.manifests[id]
+      }
+    }
+  ]...)
 
-  manifest = (
-    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-    ? sensitive(data.kustomization_overlay.provider_opentofu.manifests[each.value])
-    : data.kustomization_overlay.provider_opentofu.manifests[each.value]
-  )
-}
-
-resource "kustomization_resource" "provider_opentofu_p1" {
-  for_each = data.kustomization_overlay.provider_opentofu.ids_prio[1]
-
-  manifest = (
-    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-    ? sensitive(data.kustomization_overlay.provider_opentofu.manifests[each.value])
-    : data.kustomization_overlay.provider_opentofu.manifests[each.value]
-  )
-  wait = true
-  timeouts {
-    create = "2m"
-    update = "2m"
-  }
-
-  depends_on = [kustomization_resource.provider_opentofu_p0]
-}
-
-resource "kustomization_resource" "provider_opentofu_p2" {
-  for_each = data.kustomization_overlay.provider_opentofu.ids_prio[2]
-
-  manifest = (
-    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-    ? sensitive(data.kustomization_overlay.provider_opentofu.manifests[each.value])
-    : data.kustomization_overlay.provider_opentofu.manifests[each.value]
-  )
-
-  depends_on = [kustomization_resource.provider_opentofu_p1]
+  manifest = each.value.manifest
 }
 
 data "external" "provider_opentofu_latest_release" {
