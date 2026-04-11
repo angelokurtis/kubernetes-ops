5e8caf41c4d747886a0d225961ebfcf7eed6ac8c
Tiago Angelo <kurtis.angelo@gmail.com>
2026-04-11 10:36:40 -0300

    Update versions of kubernetes, local, proxmox, talos modules

--- proxmox-cluster/providers.tf
+++ proxmox-cluster/providers.tf
@@ -3,11 +3,11 @@ terraform {
     external   = { source = "hashicorp/external", version = "~> 2.3" }
     helm       = { source = "hashicorp/helm", version = "~> 3.1" }
     http       = { source = "hashicorp/http", version = "~> 3.5" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.38" }
-    local      = { source = "hashicorp/local", version = "~> 2.6.1" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 3.0" }
+    local      = { source = "hashicorp/local", version = "~> 2.8" }
     null       = { source = "hashicorp/null", version = "~> 3.2" }
-    proxmox    = { source = "bpg/proxmox", version = "~> 0.87" }
-    talos      = { source = "siderolabs/talos", version = "~> 0.9" }
+    proxmox    = { source = "bpg/proxmox", version = "~> 0.101" }
+    talos      = { source = "siderolabs/talos", version = "~> 0.10" }
   }
   required_version = ">= 1.9"
 }
